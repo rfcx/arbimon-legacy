83188b6fff7ce34372781f6199cf4da079ec2315
g-i-o- <aleph.omega@gmail.com>
2014-09-26 17:57:43 -0400

    visualizer code update

--- assets/js/visualizer.js
+++ assets/js/visualizer.js
@@ -1,16 +1,17 @@
-window.q={};
-(function(angular){   
-    var qt = window.q;
+//(function(angular){   
     var visualizer = angular.module('visualizer', []);
     var template_root = '/partials/visualizer/';
     var test_data = [
-        {   title   : "Recording",
+        {   title   : "Recordings",
             visible : true,
             type    : "recording-layer",
-            file    : "test-2014-08-06_12-20.wav"
+            file    : "test-2014-08-06_12-20.wav",
+            tags    : [
+                'species://coqui', 'awesome'
+            ]
         },
         {   title     : "Data Analysis",
-            visible   : true,
+            visible   : false,
             type    : "data-analysis-layer",
             sublayers : {
                 collapsed : false,
@@ -38,53 +39,63 @@ window.q={};
         }
     ];
     
+    visualizer.factory('$templateFetch', function($http, $templateCache){
+        return function $templateFetch(templateUrl, linker){
+            var template = $templateCache.get(templateUrl);
+            if(template) {
+                if (template.promise) {
+                    template.linkers.push(linker);
+                } else {
+                    linker();
+                }
+            } else {
+                var tmp_promise = {
+                    linkers : [linker],
+                    promise : $http.get(templateUrl).success(function(template){
+                        $templateCache.put(templateUrl, template);
+                        for(var i=0, l=tmp_promise.linkers, e=l.length; i < e; ++i){
+                            l[i](template);
+                        }
+                    })                        
+                };
+                $templateCache.put(templateUrl, tmp_promise);                
+            }
+        }
+    });
+    
     visualizer.controller('VisualizerCtrl', function ($scope) {
         $scope.layers = test_data; // current layers in the visualizer
     }).directive('a2Visualizer', function(){
+        
+        
         return { restrict : 'E', templateUrl: template_root + 'main.html' }
     });
     
     var layer_types = {
-        default           : true,
         'recording-layer' : true,
         'frequency-adjust-layer' : true
     };
     
-    visualizer.directive('a2VisualizerLayerItem', function($compile, $http, $templateCache){
+    visualizer.directive('a2VisualizerLayerItem', function($compile, $templateFetch){
         return {
             restrict : 'E',
-            template : '{{layer.type}}',
+            templateUrl : template_root + 'visualizer-layer-item-default.html',
             link     : function(scope, element, attrs){
-                var layer_type = layer_types[scope.layer.type] ? scope.layer.type : 'default';
-                var layer_url  = template_root + 'visualizer-layer-item-' + (layer_types[layer_type] === true ? layer_type : layer_types[layer_type]) + '.html';
-                var linker = function(layer_tmp){
-                    var layer_el   = $compile(layer_tmp)(scope);
-                    element.replaceWith(layer_el);
-                }
-                var layer_tmp  = $templateCache.get(layer_url);
-                if(layer_tmp) {
-                    if (layer_tmp.promise) {
-                        layer_tmp.linkers.push(linker);
-                    } else {
-                    }                    
-                } else {
-                    var tmp_promise = {
-                        linkers : [linker],
-                        promise : $http.get(layer_url).success(function(layer_tmp){
-                            $templateCache.put(layer_url, layer_tmp);
-                            for(var i=0, l=tmp_promise.linkers, e=l.length; i < e; ++i){
-                                l[i](layer_tmp);
-                            }
-                        })                        
-                    };
-                    $templateCache.put(layer_url, tmp_promise);
-                    
+                element.children().children().unwrap();
+                var layer_type = layer_types[scope.layer.type] ? scope.layer.type : false;
+                var layer_key  = layer_types[layer_type] === true ? layer_type : layer_types[layer_type];
+                if(layer_key && layer_key != 'default') {
+                    var layer_url  = template_root + 'visualizer-layer-item-' + layer_key + '.html';
+                    var layer_tmp  = $templateFetch(layer_url, function(layer_tmp){
+                        var layer_el   = $compile(layer_tmp)(scope);
+                        element.append(layer_el.children().unwrap());
+                    });
                 }
             }
         }
     });
     
-})(angular);
+//})(angular);
 
 /**
 
