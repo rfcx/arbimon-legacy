a64766c3a59c553ab58def8914348d5ae3a0d80c
g-i-o- <aleph.omega@gmail.com>
2014-09-29 18:05:36 -0400

    adding spectrogram layout routines

--- assets/js/visualizer.js
+++ assets/js/visualizer.js
@@ -1,43 +1,53 @@
-//(function(angular){   
+(function(angular){   
     var visualizer = angular.module('visualizer', []);
     var template_root = '/partials/visualizer/';
-    var test_data = [
-        {   title   : "Recordings",
-            visible : true,
-            type    : "recording-layer",
-            file    : "test-2014-08-06_12-20.wav",
-            tags    : [
-                'species://coqui', 'awesome'
-            ]
+    var test_data = {
+        recording : {
+            file     : "test-2014-08-06_12-20.wav",
+            duration : 60,
+            sampling_rate : 44000
         },
-        {   title     : "Data Analysis",
-            visible   : false,
-            type    : "data-analysis-layer",
-            sublayers : {
-                collapsed : false,
-                layers    : [
-                    {
-                        title   : "Regions of Interest",
-                        visible : true
-                    }, {
-                        title   : "New Model",
-                        visible : true
-                    }, {
-                        title   : "New Model Data",
-                        visible : true
-                    }
+        layers : [
+            {   title   : "Recordings",
+                visible : true,
+                type    : "recording-layer",
+                file    : "test-2014-08-06_12-20.wav",
+                tiles   : [
+                    {x:0, y:0, src:'/data/sample/test-2014-08-06_12-20.wav.bmp'}
+                ],
+                tags    : [
+                    'species://coqui', 'awesome'
                 ]
+            },
+            {   title     : "Data Analysis",
+                visible   : false,
+                type    : "data-analysis-layer",
+                sublayers : {
+                    collapsed : false,
+                    layers    : [
+                        {
+                            title   : "Regions of Interest",
+                            visible : true
+                        }, {
+                            title   : "New Model",
+                            visible : true
+                        }, {
+                            title   : "New Model Data",
+                            visible : true
+                        }
+                    ]
+                }
+            },
+            {   title   : "Frequency Filter",
+                visible : true,
+                type    : "frequency-adjust-layer",
+                params  : {
+                    minfreq : 5000,
+                    maxfreq : 15000
+                }
             }
-        },
-        {   title   : "Frequency Filter",
-            visible : true,
-            type    : "frequency-adjust-layer",
-            params  : {
-                minfreq : 5000,
-                maxfreq : 15000
-            }
-        }
-    ];
+        ]
+    };
     
     visualizer.factory('$templateFetch', function($http, $templateCache){
         return function $templateFetch(templateUrl, linker){
@@ -46,7 +56,7 @@
                 if (template.promise) {
                     template.linkers.push(linker);
                 } else {
-                    linker();
+                    linker(template);
                 }
             } else {
                 var tmp_promise = {
@@ -64,7 +74,17 @@
     });
     
     visualizer.controller('VisualizerCtrl', function ($scope) {
-        $scope.layers = test_data; // current layers in the visualizer
+        $scope.layers = test_data.layers; // current layers in the visualizer
+        $scope.layout = {
+            scale : {
+                def_sec2px :    5 / 100.0,
+                def_hz2px  : 5000 / 100.0,
+                sec2px : 100 / 1.0,
+                hz2px  : 100 / 5000.0
+            }
+        };
+        $scope.recording = test_data.recording; // current layers in the visualizer
+        $scope.recording.max_freq = $scope.recording.sampling_rate / 2;
     }).directive('a2Visualizer', function(){
         
         
@@ -75,13 +95,12 @@
         'recording-layer' : true,
         'frequency-adjust-layer' : true
     };
-    
     visualizer.directive('a2VisualizerLayerItem', function($compile, $templateFetch){
         return {
             restrict : 'E',
+            replace  : true,
             templateUrl : template_root + 'visualizer-layer-item-default.html',
             link     : function(scope, element, attrs){
-                element.children().children().unwrap();
                 var layer_type = layer_types[scope.layer.type] ? scope.layer.type : false;
                 var layer_key  = layer_types[layer_type] === true ? layer_type : layer_types[layer_type];
                 if(layer_key && layer_key != 'default') {
@@ -94,9 +113,82 @@
             }
         }
     });
+    visualizer.directive('a2VisualizerSpectrogramLayer', function($compile, $templateFetch){
+        return {
+            restrict : 'E',
+            template : '<div class="spectrogram-layer"></div>',
+            replace  : true,
+            link     : function(scope, element, attrs){
+                var layer_type = layer_types[scope.layer.type] ? scope.layer.type : false;
+                var layer_key  = layer_types[layer_type] === true ? layer_type : layer_types[layer_type];
+                element.addClass(layer_type);
+                if(layer_key && layer_key != 'default') {
+                    var layer_url  = template_root + 'visualizer-spectrogram-layer-' + layer_key + '.html';
+                    var layer_tmp  = $templateFetch(layer_url, function(layer_tmp){
+                        var layer_el   = $compile(layer_tmp)(scope);
+                        element.append(layer_el);
+                    });
+                }
+            }
+        }
+    });
+
+    visualizer.directive('a2VisualizerSpectrogram', function(){
+        var layout_tmp = {
+            defaults : {
+                rec_len : 60 // seconds
+            },
+            gutter    :  20,
+            axis_size : 100
+        }
+        return {
+            restrict : 'E',
+            templateUrl : template_root + 'visualizer-spectrogram.html',
+            replace  : true,
+            link     : function($scope, $element, $attrs){
+                var views = {
+                    viewport : $element.children('.spectrogram-container')
+                };
+                console.log('linking a2VisualizerSpectrogram ....', $scope);
+                $scope.Math = Math;
+                $scope.layout.apply = function(width, height){
+                    console.log('setting visualizer layout....', width, height, $scope);
+                    var l={};
+                    l.spectrogram = { selector : '.spectrogram-container', css:{
+                        top    : 0,
+                        left   : layout_tmp.gutter + layout_tmp.axis_size,
+                        width  : Math.ceil($scope.recording.duration * $scope.layout.scale.sec2px),
+                        height : Math.ceil($scope.recording.max_freq * $scope.layout.scale.hz2px),
+                    }};
+                    l.y_axis = { selector : '.axis-y', css:{
+                        top    : 0,
+                        left   : layout_tmp.gutter,
+                        width  : layout_tmp.axis_size,
+                        height : l.spectrogram.css.height
+                    }};
+                    l.x_axis = { selector : '.axis-x', css:{
+                        left : layout_tmp.gutter + layout_tmp.axis_size,
+                        top  : l.spectrogram.css.top + l.spectrogram.css.height,
+                        height : layout_tmp.axis_size,
+                        width  : l.spectrogram.css.width
+                    }};
+                    for(var i in l){
+                        var li = l[i];
+                        $scope.layout[i] = li.css;
+                        $element.children(li.selector).css(li.css);
+                    }
+                };
+                $scope.getElementDimensions = function () {
+                    return { 'h': $element.height(), 'w': $element.width() };
+                };
+                $scope.$watch($scope.getElementDimensions, function (newValue, oldValue) {
+                    $scope.layout.apply(newValue.w, newValue.h);
+                }, true);
+                $element.bind('resize', function () {
+                    $scope.$apply();
+                });
+            }
+        }
+    });
     
-//})(angular);
-
-/**
-
-*/
+})(angular);
