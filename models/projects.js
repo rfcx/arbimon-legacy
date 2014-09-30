bde7bfe76ed0b9df4b3de9dfe159efc3b1017cd6
g-i-o- <aleph.omega@gmail.com>
2014-09-30 14:26:26 -0400

    adding getProjectSites to project model

--- models/projects.js
+++ models/projects.js
@@ -7,6 +7,16 @@ module.exports = function(queryHandler) {
             var query = "select * from projects where url = " + mysql.escape(project_url);
             
             return queryHandler(query , callback);
+        },
+        
+        getProjectSites: function(project_id, callback){
+            var query = (
+                "SELECT S.site_id as id, S.name, S.lat, S.lon, S.site_type_id as type \n" +
+                "FROM sites S \n" +
+                "WHERE S.project_id = " + mysql.escape(project_id)
+            );
+            
+            return queryHandler(query , callback);
         }
     };
 }
