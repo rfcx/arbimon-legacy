491e885ce6594c6095c901fa5b246f32f5348c0e
Pedro A. Meléndez <lazy.chino@gmail.com>
2014-10-16 12:14:12 -0400

    update models to require dbPool and return an Object instead of function(queryHandler)

--- models/index.js
+++ models/index.js
@@ -7,10 +7,10 @@ var sha256 = require('../utils/sha256');
 
 var dbpool = require('../utils/dbpool');
 
-module.exports.users         = require('./users'        )(dbpool.queryHandler);
-module.exports.projects      = require('./projects'     );
-module.exports.recordings    = require('./recordings'   );
-module.exports.sites         = require('./sites'        )(dbpool.queryHandler);
+module.exports.users         = require('./users');
+module.exports.projects      = require('./projects');
+module.exports.recordings    = require('./recordings');
+module.exports.sites         = require('./sites');
 module.exports.training_sets = require('./training_sets');
-module.exports.species       = require('./species'      )(dbpool.queryHandler);
-module.exports.songtypes     = require('./songtypes'    )(dbpool.queryHandler);
+module.exports.species       = require('./species');
+module.exports.songtypes     = require('./songtypes');
