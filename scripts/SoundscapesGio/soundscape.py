2ad973e47a35f62f3520debe7b787a35d5e493a5
g-i-o- <aleph.omega@gmail.com>
2014-11-12 16:44:26 -0400

    now uses bmpio module for making bitmaps. if extension is ".png", then can make a png file (uses pypng). Soundscape get read out of an open file handle now instead of a string

--- scripts/SoundscapesGio/soundscape.py
+++ scripts/SoundscapesGio/soundscape.py
@@ -1,4 +1,5 @@
-import struct
+import bmpio
+import png
 import colors
 import scidx
 from datetime import datetime
@@ -27,7 +28,7 @@ aggregations = {
 
 
 class Soundscape():
-    def __init__(self, csv, aggregation, bin_size, max_bins):
+    def __init__(self, finp, aggregation, bin_size, max_bins):
         "Constructs a soundscape from a peaks file"
         self.aggregation = aggregation
         self.start_bin = 0
@@ -43,7 +44,7 @@ class Soundscape():
             'max_count': 0
         }
 
-        for i in self.get_peak_list(csv):
+        for i in self.get_peak_list(finp):
             i_bin, i_idx, i_id = (i['bin'], i['idx'], i['id'])
             stats['min_idx'] = min(stats['min_idx'], i_idx)
             stats['max_idx'] = max(stats['max_idx'], i_idx)
@@ -68,87 +69,80 @@ class Soundscape():
         self.bins = bins
         self.stats = stats
 
-    def get_peak_list(self, csv):
+    def get_peak_list(self, finp):
         "Generator that reads a file and yields peaks in an aggregated form"
         aggregation = self.aggregation
         max_bins = self.max_bins
         bin_size = self.bin_size
         hwhitelist = ["date", "id", "PeaksFrec"]
         header = []
-        with open(csv, "r") as finp:
-            for i, l in enumerate(finp):
-                if i == 0:
-                    headers = [x.strip() for x in l.split(',')]
-                else:
-                    l = dict([(x, y) for (x, y) in zip(
-                        headers,
-                        [x.strip() for x in l.split(',')]
-                    ) if x in hwhitelist])
-                    date = datetime.strptime(l['date'], '%m/%d/%Y %I:%M %p')
-                    l['idx'] = int(sum([
-                        float(date.strftime(x)) * y for (x, y) in
-                        zip(aggregation['date'], aggregation['projection'])
-                    ]))
-                    l['bin'] = min(
-                        int(float(l['PeaksFrec']) * 1000 / bin_size),
-                        max_bins
-                    )
+        for i, l in enumerate(finp):
+            if i == 0:
+                headers = [x.strip() for x in l.split(',')]
+            else:
+                l = dict([(x, y) for (x, y) in zip(
+                    headers,
+                    [x.strip() for x in l.split(',')]
+                ) if x in hwhitelist])
+                date = datetime.strptime(l['date'], '%m/%d/%Y %I:%M %p')
+                l['idx'] = int(sum([
+                    float(date.strftime(x)) * y for (x, y) in
+                    zip(aggregation['date'], aggregation['projection'])
+                ]))
+                l['bin'] = min(
+                    int(float(l['PeaksFrec']) * 1000 / bin_size),
+                    max_bins
+                )
 
-                    del l['date']
-                    del l['PeaksFrec']
-                    yield l
+                del l['date']
+                del l['PeaksFrec']
+                yield l
+
+    @staticmethod
+    def cols_gen(bin, scale, from_x, to_x):
+        "yields counts for each column in a cell"
+        for x in range(from_x, to_x):
+            v = len(bin[x]) if bin and x in bin else 0
+            yield int(v * scale)
+
+    @classmethod
+    def rows_gen(cls, bins, scale, from_y, to_y, from_x, to_x):
+        "yields column iterators for each row in the index"
+        for y in range(to_y-1, from_y-1, -1):
+            yield cls.cols_gen(bins.get(y), scale, from_x, to_x)
 
     def write_image(self, imgout):
         "Writes the soundscape to an image file"
-        bins = self.bins
-        max_count = self.stats['max_count']
+
         agg_range = self.aggregation['range']
         if agg_range == 'auto':
-            offsetx = self.stats['min_idx']
-            width = self.stats['max_idx'] - offsetx + 1
-        else:
-            offsetx = agg_range[0]
-            width = agg_range[1] - offsetx + 1
-        bits_per_pixel = 8
-        rowsize = int((bits_per_pixel * width + 31)/32)*4
-        wpadding = rowsize - width
+            agg_range = [self.stats['min_idx'], self.stats['max_idx']]
+
+        offsetx = agg_range[0]
+        width = agg_range[1] - offsetx + 1
         height = self.max_bins
-        img_size = rowsize * height * bits_per_pixel/8
-        pallette_size = 256 * 4
-        img_start_off = 40 + 14 + pallette_size
-        file_size = img_start_off + img_size
-        header = [
-            "BM", file_size, 0, 0, img_start_off,
-            40, width, -height, 1, bits_per_pixel, 0, img_size,
-            100, 100,
-            256, 0
-        ]
+        bpp = 8
 
-        # print header, rowsize, wpadding
-
-        fout = file(imgout, "wb")
-        fout.write(struct.pack("<2sIHHIIiiHHIIiiII", *header))
         gradient = colors.Gradient(
             [[240, 100, 57], [17, 100, 98]],
             [360, 100, 100], 255, spacetx=colors.hsv2rgb
         )
-        for i in range(256):
-            r, g, b = gradient(i / 255.0)
-            fout.write(struct.pack("<BBBB", b, g, r, 0))
+        palette = [gradient(i / 255.0) for i in range(256)]
 
-        for y in range(height-1, -1, -1):
-            for x in range(offsetx, offsetx + width):
-                v, c = None, 0
-                if y in bins:
-                    bin = bins[y]
-                    if x in bin:
-                        v = bin[x]
-                        c = int(len(v) * 255 / max_count)
-                # print (v, c),
-                fout.write(struct.pack("<B", c))
-            for x in range(0, wpadding):
-                fout.write(struct.pack("<B", 0))
-            # print
+        if imgout[:4] == ".png":
+            w = pypng.Writer(
+                width=width, height=height, bitdepth=bpp, palette=palette
+            )
+        else:
+            w = bmpio.Writer(
+                width=width, height=height, bitdepth=bpp, palette=palette
+            )
+        fout = file(imgout, "wb")
+
+        w.write(fout, self.rows_gen(
+            self.bins, 255.0 / self.stats['max_count'],
+            0, height, offsetx, offsetx + width
+        ))
 
     def write_index(self, indexout):
         bins = self.bins
