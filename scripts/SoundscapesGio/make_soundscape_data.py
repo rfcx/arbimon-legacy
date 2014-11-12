03c4753f0a8e2a525b85bd97eb4f37bc2f21d209
g-i-o- <aleph.omega@gmail.com>
2014-11-12 16:48:44 -0400

    peaks file can now be stdin (argument must be set to "-") imageout can now be a png file (bmp or png is selected by the file's extension) code was linted with pep8

--- scripts/SoundscapesGio/make_soundscape_data.py
+++ scripts/SoundscapesGio/make_soundscape_data.py
@@ -3,29 +3,29 @@ import soundscape
 
 
 USAGE = """
-{prog} peaks.csv max_hertz bin_size aggregation imageout.bmp scidxout.scidx
-    peaks.csv - peaks file
+{prog} peaks.csv max_hertz bin_size aggregation imageout.png scidxout.scidx
+    peaks.csv - peaks file (can be "-" for stdin)
     max_hertz - maximum hertz to represent
     bin_size  - size of bins in hertz
     aggregation - one of : {aggregations}
-    imageout.bmp - image file (bmp format)
+    imageout.png - image output file (.png or .bmp format)
+    scidxout.scidx - index output file (scidx format)
 """.format(
-    prog = sys.argv[0],
-    aggregations = ', '.join(soundscape.aggregations.keys())
+    prog=sys.argv[0],
+    aggregations=', '.join(soundscape.aggregations.keys())
 )
-
 
 
 if len(sys.argv) < 7:
     print USAGE
     sys.exit()
-    
-csv         = sys.argv[1]
-max_hertz   = int(sys.argv[2])
-bin_size    = int(sys.argv[3])
+
+peaks_file = sys.argv[1]
+max_hertz = int(sys.argv[2])
+bin_size = int(sys.argv[3])
 aggregation = soundscape.aggregations.get(sys.argv[4])
-imgout      = sys.argv[5]
-scidxout    = sys.argv[6]
+imgout = sys.argv[5]
+scidxout = sys.argv[6]
 
 max_bins = int(max_hertz / bin_size)
 
@@ -34,13 +34,17 @@ if not aggregation:
     print USAGE
     sys.exit()
 
-    
+using_file = (peaks_file != "-")
 
-        
+try:
+    finp = open(peaks_file, "r") if using_file else sys.stdin
+    scp = soundscape.Soundscape(finp, aggregation, bin_size, max_bins)
+finally:
+    if using_file:
+        finp.close()
 
-scp = soundscape.Soundscape(csv, aggregation, bin_size, max_bins)
-scp.write_image(imgout);
-scp.write_index(scidxout);
+scp.write_image(imgout)
+scp.write_index(scidxout)
 
 # print scp.recordings, scp.stats
 # print aggregation['range']
