8ead501032fff9c29f6e7e88419ad1fa22275f18
g-i-o- <aleph.omega@gmail.com>
2014-11-12 11:56:54 -0400

    linting sounscape files

--- scripts/SoundscapesGio/scidx.py
+++ scripts/SoundscapesGio/scidx.py
@@ -3,36 +3,41 @@ import math
 
 VERSION = 1
 
+
 class file_pointer_write_loc(object):
     def __init__(self, f, relative_pos=0):
         self.f = f
-        self.pos = f.tell();
+        self.pos = f.tell()
         self.relative_pos = relative_pos
         f.write(struct.pack(">Q", 0))
+
     def update(self):
         f = self.f
-        pos = f.tell();
+        pos = f.tell()
         wpos = pos
         if self.relative_pos:
             wpos -= self.relative_pos
-                
+
         f.seek(self.pos)
         f.write(struct.pack(">Q", wpos))
         f.seek(pos)
 
+
 def uint2BEbytes(x, n):
-    return [(x >> (8*i)) & 0xff for i in range(n-1,-1,-1)]
+    return [(x >> (8*i)) & 0xff for i in range(n-1, -1, -1)]
+
+
 def BEbytes2uint(b):
     return sum([long(b[i]) << (8*(len(b) - 1 - i)) for i in range(len(b))])
 
 
 def write_scidx(filename, index, recordings, offsetx, width, offsety, height):
     "Writes a scidx file to the given filename, given the index attributes"
-    fout = file(filename, "wb");
+    fout = file(filename, "wb")
     rcount = len(recordings)
     rcbytes = int(math.ceil(math.log(rcount, 2)/8.0))
     rcfmt = ">" + ("B"*rcbytes)
-    rec_idx={}
+    rec_idx = {}
     # write header
     # file :
     #     field 	desc
@@ -42,11 +47,16 @@ def write_scidx(filename, index, recordings, offsetx, width, offsety, height):
     #     height 	2 byte uint
     #     rec_count 	3 byte uint
     #     rec_bytes 	1 byte uint
-    #     rows_ptr_start 	array of 8 byte file offset from start - indicates begining of rows_ptr[] array
+    #     rows_ptr_start 	array of 8 byte file offset from start - indicates
+    #                       begining of rows_ptr[] array
     #     recs[] 	array of rec structures of size rec_count
-    #     rows_ptr[] 	array of 8 byte file offset from start - indicates begining of each entry of rows[]
+    #     rows_ptr[] 	array of 8 byte file offset from start - indicates
+    #                       begining of each entry of rows[]
     #     rows[] 	array of row structures of size height
-    fout.write(struct.pack(">6sHHHHH", "SCIDX ", VERSION, offsetx, width, offsety, height))
+    fout.write(struct.pack(
+        ">6sHHHHH",
+        "SCIDX ", VERSION, offsetx, width, offsety, height
+    ))
     fout.write(struct.pack(">BBB", *uint2BEbytes(rcount, 3)))
     fout.write(struct.pack(">B", rcbytes))
     # setup file_pointer_write_loc for writing start of row pointers array
@@ -65,7 +75,9 @@ def write_scidx(filename, index, recordings, offsetx, width, offsety, height):
     # write each row
     # row structure :
     #     field 	desc
-    #     cells_ptr[] 	array of 8 byte offsets from the beginning of the row structure - indicates begining of each entry of cells[]
+    #     cells_ptr[] 	array of 8 byte offsets from the beginning of the
+    #                   row structure - indicates begining of each entry of
+    #                   cells[]
     #     cells[] 	array of cell structures of size width
     for y in range(height):
         off_y = offsety + y
@@ -75,28 +87,43 @@ def write_scidx(filename, index, recordings, offsetx, width, offsety, height):
             row_pointers[y].update()
             row = index[off_y]
             # setup an array of positions relative to row (one per cell)
-            cell_pointers = [file_pointer_write_loc(fout, row_pointers[y].pos) for x in range(width)]
+            cell_pointers = [
+                file_pointer_write_loc(fout, row_pointers[y].pos)
+                for x in range(width)
+            ]
             # write each cell
             # cell structure :
             #     field 	desc
             #     count 	2 byte uint - number of recordings in indices[]
-            #     indices[] 	array of rec_bytes byte uints of size count - indicates indices in recs[] lists
+            #     indices[] 	array of rec_bytes byte uints of size count -
+            #                   indicates indices in recs[] lists
             for x in range(width):
                 off_x = offsetx + x
                 # if the row is in the index, then:::
                 if off_x in row:
-                    # update the cell_pointers[x] location to the current file position, relative to row_pointers[y]
+                    # update the cell_pointers[x] location to the current
+                    # file position, relative to row_pointers[y]
                     cell_pointers[x].update()
                     cell = row[off_x]
                     fout.write(struct.pack(">H", len(cell)))
                     for rec in cell:
-                        fout.write(struct.pack(rcfmt, *uint2BEbytes(rec_idx[rec], rcbytes)))
+                        fout.write(struct.pack(
+                            rcfmt, *uint2BEbytes(rec_idx[rec], rcbytes)
+                        ))
 
-def read_scidx(filename):
+
+def read_scidx(filename, filter=None):
     "Reads a scidx file, given the filename"
     index = {}
-    
-    fin = file(filename, "rb");
+
+    if not filter:
+        filter = {}
+    minx, maxx, miny, maxy = [filter.get(f, v) for f, v in zip(
+        ['minx', 'maxx', 'miny', 'maxy'],
+        [float('-inf'), float('inf'), float('-inf'), float('inf')],
+    )]
+
+    fin = file(filename, "rb")
     # read header
     # file :
     #     field 	desc
@@ -106,12 +133,23 @@ def read_scidx(filename):
     #     height 	2 byte uint
     #     rec_count 	3 byte uint
     #     rec_bytes 	1 byte uint
-    #     rows_ptr_start 	array of 8 byte file offset from start - indicates begining of rows_ptr[] array
+    #     rows_ptr_start 	array of 8 byte file offset from start - indicates
+    #                       begining of rows_ptr[] array
     #     recs[] 	array of rec structures of size rec_count
-    #     rows_ptr[] 	array of 8 byte file offset from start - indicates begining of each entry of rows[]
+    #     rows_ptr[] 	array of 8 byte file offset from start - indicates
+    #                   begining of each entry of rows[]
     #     rows[] 	array of row structures of size height
-    magic, VERSION, offsetx, width, offsety, height = struct.unpack(">6sHHHHH", fin.read(12))
-    rcount   = BEbytes2uint(struct.unpack(">BBB", fin.read(3)))
+    magic, VERSION, offsetx, width, offsety, height = struct.unpack(
+        ">6sHHHHH", fin.read(12)
+    )
+
+    if not filter.get("ignore_offsets", False):
+        minx -= offsetx
+        maxx -= offsetx
+        miny -= offsety
+        maxy -= offsety
+
+    rcount = BEbytes2uint(struct.unpack(">BBB", fin.read(3)))
     rcbytes, = struct.unpack(">B", fin.read(1))
     rcfmt = ">" + ("B"*rcbytes)
     # read pointer of start of row pointers array
@@ -128,28 +166,37 @@ def read_scidx(filename):
     # read each row
     # row structure :
     #     field 	desc
-    #     cells_ptr[] 	array of 8 byte offsets from the beginning of the row structure - indicates begining of each entry of cells[]
+    #     cells_ptr[] 	array of 8 byte offsets from the beginning of the row
+    #                   structure - indicates begining of each entry of cells[]
     #     cells[] 	array of cell structures of size width
     for y in range(height):
-        if row_pointers[y]:
+        if row_pointers[y] and miny <= y <= maxy:
             # seek to the location of the current row
             finp.seek(row_pointers[y])
             off_y = offsety + y
             row = {}
             index[off_y] = row
             # read array positions relative to row (one per cell)
-            cell_pointers = [struct.unpack(">Q", fin.read(8)) for y in range(width)]
+            cell_pointers = [
+                struct.unpack(">Q", fin.read(8))
+                for y in range(width)
+            ]
             # read each cell
             # cell structure :
             #     field 	desc
             #     count 	2 byte uint - number of recordings in indices[]
-            #     indices[] 	array of rec_bytes byte uints of size count - indicates indices in recs[] lists
-            for x in range(width):
+            #     indices[] 	array of rec_bytes byte uints of size count -
+            #                   indicates indices in recs[] lists
+            for x in range(width) and minx <= x <= maxx:
                 if cell_pointers[x]:
-                    # seek to cell location denoted by cell_pointers[x], relative to row_pointers[y]
+                    # seek to cell location denoted by cell_pointers[x],
+                    # relative to row_pointers[y]
                     fin.seek(cell_pointers[x] + row_pointers[y])
                     cell_count, = struct.unpack(">H", fin.read(2))
                     row[offsetx + x] = [
-                        recordings[BEbytes2uint(struct.pack(rcfmt, fin.read(rcbytes)))]
+                        recordings[BEbytes2uint(struct.pack(
+                            rcfmt, fin.read(rcbytes)
+                        ))]
                         for i in range(cell_count)
-                    ]    
+                    ]
+    return (index, recordings, offsetx, width, offsety, height)
--- scripts/SoundscapesGio/soundscape.py
+++ scripts/SoundscapesGio/soundscape.py
@@ -5,48 +5,49 @@ from datetime import datetime
 
 
 aggregations = {
-    'time_of_day'   : {
-        'date' : ['%H'], 'projection' : [1], 'range': [0, 23]
+    'time_of_day': {
+        'date': ['%H'], 'projection': [1], 'range': [0,  23]
     },
-    'day_of_month'  : {
-        'date' : ['%d'], 'projection' : [1], 'range': [1, 31]
+    'day_of_month': {
+        'date': ['%d'], 'projection': [1], 'range': [1,  31]
     },
-    'day_of_year'   : {
-        'date' : ['%j'], 'projection' : [1], 'range': [1,366]
+    'day_of_year': {
+        'date': ['%j'], 'projection': [1], 'range': [1, 366]
     },
-    'month_in_year' : {
-        'date' : ['%m'], 'projection' : [1], 'range': [1, 12]
+    'month_in_year': {
+        'date': ['%m'], 'projection': [1], 'range': [1,  12]
     },
-    'day_of_week'   : {
-        'date' : ['%w'], 'projection' : [1], 'range': [0,  6]
+    'day_of_week': {
+        'date': ['%w'], 'projection': [1], 'range': [0,   6]
     },
-    'year'          : {
-        'date' : ['%Y'], 'projection' : [1], 'range': 'auto'
+    'year': {
+        'date': ['%Y'], 'projection': [1], 'range': 'auto'
     }
 }
+
 
 class Soundscape():
     def __init__(self, csv, aggregation, bin_size, max_bins):
         "Constructs a soundscape from a peaks file"
         self.aggregation = aggregation
         self.start_bin = 0
-        self.max_bins    = max_bins
-        self.bin_size    = bin_size
-        
+        self.max_bins = max_bins
+        self.bin_size = bin_size
+
         bins = {}
         recordings = {}
         max_list = None
         stats = {
-            'min_idx' : float('inf'),
-            'max_idx' : float('-inf'),
+            'min_idx': float('inf'),
+            'max_idx': float('-inf'),
             'max_count': 0
         }
-        
+
         for i in self.get_peak_list(csv):
             i_bin, i_idx, i_id = (i['bin'], i['idx'], i['id'])
             stats['min_idx'] = min(stats['min_idx'], i_idx)
             stats['max_idx'] = max(stats['max_idx'], i_idx)
-            
+
             if i_id not in recordings:
                 recordings[i_id] = 1
             if i_bin not in bins:
@@ -60,37 +61,43 @@ class Soundscape():
 
             if not max_list or len(max_list) < len(recs):
                 max_list = recs
-                
-        stats['max_count'] = len(max_list) if max_list else 0;
+
+        stats['max_count'] = len(max_list) if max_list else 0
         self.recordings = recordings.keys()
         self.recordings.sort()
         self.bins = bins
         self.stats = stats
-        
+
     def get_peak_list(self, csv):
         "Generator that reads a file and yields peaks in an aggregated form"
         aggregation = self.aggregation
         max_bins = self.max_bins
         bin_size = self.bin_size
         hwhitelist = ["date", "id", "PeaksFrec"]
-        header=[]
+        header = []
         with open(csv, "r") as finp:
             for i, l in enumerate(finp):
                 if i == 0:
-                    headers=[x.strip() for x in l.split(',')]
+                    headers = [x.strip() for x in l.split(',')]
                 else:
-                    l = dict([(x,y) for (x,y) in zip(
+                    l = dict([(x, y) for (x, y) in zip(
                         headers,
                         [x.strip() for x in l.split(',')]
                     ) if x in hwhitelist])
                     date = datetime.strptime(l['date'], '%m/%d/%Y %I:%M %p')
-                    l['idx'] = int(sum([float(date.strftime(x)) * y for (x,y) in zip(aggregation['date'], aggregation['projection'])]))                
-                    l['bin']  = min(int(float(l['PeaksFrec']) *  1000 / bin_size), max_bins)
-                    
+                    l['idx'] = int(sum([
+                        float(date.strftime(x)) * y for (x, y) in
+                        zip(aggregation['date'], aggregation['projection'])
+                    ]))
+                    l['bin'] = min(
+                        int(float(l['PeaksFrec']) * 1000 / bin_size),
+                        max_bins
+                    )
+
                     del l['date']
                     del l['PeaksFrec']
                     yield l
-                    
+
     def write_image(self, imgout):
         "Writes the soundscape to an image file"
         bins = self.bins
@@ -98,37 +105,37 @@ class Soundscape():
         agg_range = self.aggregation['range']
         if agg_range == 'auto':
             offsetx = self.stats['min_idx']
-            width   = self.stats['max_idx'] - offsetx + 1
-        else :
+            width = self.stats['max_idx'] - offsetx + 1
+        else:
             offsetx = agg_range[0]
-            width   = agg_range[1] - offsetx + 1
-        bits_per_pixel =  8
-        rowsize  = int((bits_per_pixel * width + 31)/32)*4
+            width = agg_range[1] - offsetx + 1
+        bits_per_pixel = 8
+        rowsize = int((bits_per_pixel * width + 31)/32)*4
         wpadding = rowsize - width
         height = self.max_bins
-        img_size       = rowsize * height * bits_per_pixel/8
-        pallette_size  = 256 * 4
-        img_start_off  = 40 + 14 + pallette_size
-        file_size      = img_start_off + img_size
+        img_size = rowsize * height * bits_per_pixel/8
+        pallette_size = 256 * 4
+        img_start_off = 40 + 14 + pallette_size
+        file_size = img_start_off + img_size
         header = [
             "BM", file_size, 0, 0, img_start_off,
             40, width, -height, 1, bits_per_pixel, 0, img_size,
             100, 100,
             256, 0
         ]
-        
+
         # print header, rowsize, wpadding
-        
+
         fout = file(imgout, "wb")
         fout.write(struct.pack("<2sIHHIIiiHHIIiiII", *header))
-        gradient = colors.Gradient([
-            [240,100, 57], 
-            [ 17, 100, 98]
-        ], [360,100,100], 255, spacetx = colors.hsv2rgb)
+        gradient = colors.Gradient(
+            [[240, 100, 57], [17, 100, 98]],
+            [360, 100, 100], 255, spacetx=colors.hsv2rgb
+        )
         for i in range(256):
             r, g, b = gradient(i / 255.0)
-            fout.write(struct.pack("<BBBB", b, g, r, 0));
-            
+            fout.write(struct.pack("<BBBB", b, g, r, 0))
+
         for y in range(height-1, -1, -1):
             for x in range(offsetx, offsetx + width):
                 v, c = None, 0
@@ -136,13 +143,13 @@ class Soundscape():
                     bin = bins[y]
                     if x in bin:
                         v = bin[x]
-                        c = int(len(v) * 255 / max_count);
+                        c = int(len(v) * 255 / max_count)
                 # print (v, c),
-                fout.write(struct.pack("<B", c));
+                fout.write(struct.pack("<B", c))
             for x in range(0, wpadding):
-                fout.write(struct.pack("<B", 0));
+                fout.write(struct.pack("<B", 0))
             # print
-    
+
     def write_index(self, indexout):
         bins = self.bins
         recordings = self.recordings
@@ -150,12 +157,14 @@ class Soundscape():
         agg_range = self.aggregation['range']
         if agg_range == 'auto':
             offsetx = self.stats['min_idx']
-            width   = self.stats['max_idx'] - offsetx + 1
-        else :
+            width = self.stats['max_idx'] - offsetx + 1
+        else:
             offsetx = agg_range[0]
-            width   = agg_range[1] - offsetx + 1
+            width = agg_range[1] - offsetx + 1
         offsety = 0
-        height  = self.max_bins
-        
-        scidx.write_scidx(indexout, bins, recordings, offsetx, width, offsety, height)
-        
+        height = self.max_bins
+
+        scidx.write_scidx(
+            indexout, bins, recordings,
+            offsetx, width, offsety, height
+        )
