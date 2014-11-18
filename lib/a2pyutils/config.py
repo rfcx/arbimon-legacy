1c024e858dcf4b1483bc432b45f12537ea80761f
g-i-o- <aleph.omega@gmail.com>
2014-11-18 18:34:43 -0400

    improved config class to auto-load configs as needed

--- lib/a2pyutils/config.py
+++ lib/a2pyutils/config.py
@@ -1,26 +1,56 @@
 #!/usr/bin/env python
 import json
 import os
+import os.path
 
-if os.path.isfile('config/db.local.json'):
-    db_config_path = 'config/db.local.json'
-else:
-    db_config_path = 'config/db.json'
 
-class Config:
+class Config(object):
+    scache = {}
 
-    def __init__(self):
-        aws_data=open('config/aws.json')
-        self.awsConfig = json.load(aws_data)        
-        db_data=open(db_config_path)
-        self.dbConfig = json.load(db_data)
-    
+    def __init__(self, basepath=None):
+        self.cache = {}
+
+        self.basepath = basepath if basepath else "config"
+
+        self.load('aws')
+        self.load('db')
+
+    @classmethod
+    def for_path(cls, basepath):
+        if basepath not in cls.scache:
+            cls.scache[basepath] = cls(basepath)
+        return cls.scache[basepath]
+
     def data(self):
-        return [self.dbConfig['host'],
-                self.dbConfig['user'],
-                self.dbConfig['password'],
-                self.dbConfig['database'],
-                self.awsConfig['bucketName'],
-                self.awsConfig['accessKeyId'],
-                self.awsConfig['secretAccessKey']
-                ]
+        return [
+            self.dbConfig['host'],
+            self.dbConfig['user'],
+            self.dbConfig['password'],
+            self.dbConfig['database'],
+            self.awsConfig['bucketName'],
+            self.awsConfig['accessKeyId'],
+            self.awsConfig['secretAccessKey']
+        ]
+
+    def load(self, cfg):
+        cfgbasepath = os.path.join(self.basepath, cfg)
+
+        if os.path.isfile(cfgbasepath + '.local.json'):
+            cfgpath = cfgbasepath + '.local.json'
+        else:
+            cfgpath = cfgbasepath + '.json'
+
+        with open(cfgpath) as filedata:
+            config = json.load(filedata)
+
+        attr = cfg + "Config"
+        self.cache[cfg] = config
+        return config
+
+    def __getattr__(self, identifier):
+        cfg, magic = identifier[:-6], identifier[-6:]
+        if cfg and magic == "Config":
+            if cfg not in self.cache:
+                return self.load(cfg)
+            else:
+                return self.cache[cfg]
