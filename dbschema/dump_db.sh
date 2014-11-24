c83934807510f0dd355d9de2fe1bc9f4da85785e
g-i-o- <aleph.omega@gmail.com>
2014-11-24 17:43:29 -0400

    now auto_increment values are ommited in db structure dumps

--- dbschema/dump_db.sh
+++ dbschema/dump_db.sh
@@ -2,6 +2,6 @@
 db=arbimon2
 
 echo "Dumping $db structure"
-mysqldump -p -d arbimon2 > $db-structure.sql
+mysqldump -p -d arbimon2 | sed 's/AUTO_INCREMENT=[0-9][0-9]*//' > $db-structure.sql
 echo "Dumping $db data"
 mysqldump -p -t arbimon2 > $db-data.sql
