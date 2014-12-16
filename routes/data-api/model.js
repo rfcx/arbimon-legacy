fe07ca59340c38bc2d901c7597822cd59c7606b1
g-i-o- <aleph.omega@gmail.com>
2014-12-16 15:20:56 -0400

    soundscape now depends on the job queue

--- routes/data-api/model.js
+++ routes/data-api/model.js
@@ -589,47 +589,6 @@ router.post('/project/:projectUrl/soundscape/new', function(req, res, next) {
             var next = arguments[arguments.length -1];
             job_id = _job_id;
             next();
-        },
-        function push_job_to_queue(row){
-            var next = arguments[arguments.length -1];
-            jobQueue.push({
-                name: 'soundscapeJob'+job_id,
-                work: function(callback){
-                    var cmd = __dirname+'/../../.env/bin/python', args = [
-                        scriptsFolder+'Soundscapes/playlist2soundscape.py' ,
-                        cmd_escape(job_id           ), cmd_escape(params.playlist ),
-                        cmd_escape(params.maxhertz  ), cmd_escape(params.bin      ), cmd_escape(params.aggregation),
-                        cmd_escape(params.threshold ), cmd_escape(params.project  ), cmd_escape(params.user       ),
-                        cmd_escape(params.name      ), cmd_escape(params.frequency)
-                    ];
-                    var python = require('child_process').spawn(cmd, args);
-                    var output = "";
-                    python.stdout.on('data', function(data){ 
-                        output += data
-                    });
-                    python.on('close', function(code){ 
-                        if (code !== 0) { 
-                            debug('soundscapeJob '+job_id+' returned error ' + code);
-                            debug('cmd : ' + cmd);
-                            debug('args  : ' + args);
-                        } else {
-                            debug('no error, everything ok, soundscapeJob completed ',job_id);
-                        }
-                        callback();
-                    });
-                }
-            },
-            1,
-            function() {
-                debug("job done! soundscapeJob:", soundscapeId);
-                model.projects.insertNews({
-                    news_type_id: 11, // soundscape created
-                    user_id: req.session.user.id,
-                    project_id: project_id,
-                    data: JSON.stringify({ soundscape: name })
-                });
-            });
-            next();
         }
     ], function(err, job_id){
         if(err){
