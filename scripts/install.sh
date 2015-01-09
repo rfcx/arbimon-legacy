382e3bd9e272d4ef18ba421cbb5ca6c88c0d8de6
g-i-o- <aleph.omega@gmail.com>
2015-01-09 10:14:21 -0400

    added -y flags to aptget

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-sudo apt-get install libcap2-bin git
+sudo apt-get install -y libcap2-bin git
 
 repo="chris-lea/node.js"
 repoppa="ppa:$repo"
@@ -10,7 +10,7 @@ if ! (grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep -v list.save
 fi
 
 sudo apt-get update
-sudo apt-get install python-pip sox libsox-fmt-mp3 imagemagick nodejs libmysqlclient-dev python-dev gfortran libopenblas-dev liblapack-dev  libpng12-dev libfreetype6-dev libsndfile1 libsndfile-dev python-virtualenv r-base r-base-dev libfftw3-3 libfftw3-dev r-cran-rgl bwidget
+sudo apt-get install -y python-pip sox libsox-fmt-mp3 imagemagick nodejs libmysqlclient-dev python-dev gfortran libopenblas-dev liblapack-dev  libpng12-dev libfreetype6-dev libsndfile1 libsndfile-dev python-virtualenv r-base r-base-dev libfftw3-3 libfftw3-dev r-cran-rgl bwidget
 sudo scripts/setup/r-packages.R
 sudo setcap cap_net_bind_service=+ep /usr/bin/nodejs
 sudo npm install -g grunt-cli
