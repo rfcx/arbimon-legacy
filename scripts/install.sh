0ea91dadde85ef862c94b7cb449066d9c4b9acdd
rafael-alvarez <rafael.alvarez.berrios@gmail.com>
2015-01-13 09:33:08 -0400

    Removed jobs
    Removed jobs folders and dependencies
    Enviroment changes, lighter enviroment
    
     Changes to be committed:
    	modified:   README.md
    	deleted:    lib/a2audio/__init__.py
    	deleted:    lib/a2audio/model.py
    	deleted:    lib/a2audio/rec.py
    	deleted:    lib/a2audio/recanalizer.py
    	deleted:    lib/a2audio/roiset.py
    	deleted:    lib/a2audio/roizer.py
    	deleted:    lib/a2pyutils/__init__.py
    	deleted:    lib/a2pyutils/bmpio.py
    	deleted:    lib/a2pyutils/colors.py
    	deleted:    lib/a2pyutils/config.py
    	deleted:    lib/a2pyutils/logger.py
    	deleted:    lib/a2pyutils/news.py
    	deleted:    lib/a2pyutils/palette.py
    	deleted:    lib/a2pyutils/plotter.py
    	deleted:    lib/a2pyutils/tempfilecache.py
    	deleted:    lib/soundscape/__init__.py
    	deleted:    lib/soundscape/scidx.py
    	deleted:    lib/soundscape/soundscape.py
    	modified:   production-setup.md
    	modified:   requirements.txt
    	deleted:    scripts/PatternMatching/audiomapper/.gitignore
    	deleted:    scripts/PatternMatching/audiomapper/HOWTORUN
    	deleted:    scripts/PatternMatching/audiomapper/__init__.py
    	deleted:    scripts/PatternMatching/audiomapper/align.py
    	deleted:    scripts/PatternMatching/audiomapper/classificationresults.py
    	deleted:    scripts/PatternMatching/audiomapper/classifyMap.py
    	deleted:    scripts/PatternMatching/audiomapper/modelize.py
    	deleted:    scripts/PatternMatching/audiomapper/recClassify.py
    	deleted:    scripts/PatternMatching/audiomapper/recnilize.py
    	deleted:    scripts/PatternMatching/audiomapper/roigen.py
    	deleted:    scripts/PatternMatching/audiomapper/trainMap.py
    	deleted:    scripts/PatternMatching/classification.py
    	deleted:    scripts/PatternMatching/training.py
    	deleted:    scripts/Soundscapes/csv2soundscape.py
    	deleted:    scripts/Soundscapes/fpeaks.R
    	deleted:    scripts/Soundscapes/playlist2soundscape.py
    	modified:   scripts/install.sh
    	deleted:    scripts/jobs/run_classification_job.py
    	deleted:    scripts/jobs/run_training_job.py
    	deleted:    scripts/setup/r-packages.R

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,8 +10,7 @@ if ! (grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep -v list.save
 fi
 
 sudo apt-get update
-sudo apt-get install -y python-pip sox libsox-fmt-mp3 imagemagick nodejs libmysqlclient-dev python-dev gfortran libopenblas-dev liblapack-dev  libpng12-dev libfreetype6-dev libsndfile1 libsndfile-dev python-virtualenv r-base r-base-dev libfftw3-3 libfftw3-dev r-cran-rgl bwidget
-sudo scripts/setup/r-packages.R
+sudo apt-get install -y python-pip sox libsox-fmt-mp3 imagemagick nodejs libmysqlclient-dev python-dev libpng12-dev libfreetype6-dev python-virtualenv
 sudo setcap cap_net_bind_service=+ep /usr/bin/nodejs
 sudo npm install -g grunt-cli
 sudo npm install -g bower
--- scripts/setup/r-packages.R
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/Rscript --vanilla --default-packages=utils
-install.packages("tuneR",repos='http://cran.us.r-project.org')
-install.packages("seewave",repos='http://cran.us.r-project.org')
