31de8ca08eaaa3dbbcff2f7236b2cb2f095df429
rafael-alvarez <rafael.alvarez.berrios@gmail.com>
2015-01-30 23:45:30 -0400

    Lib folder was missing.
    Some files are needed for the set_visual_scale script
    
     Changes to be committed:
    	new file:   lib/a2audio/__init__.py
    	new file:   lib/a2audio/model.py
    	new file:   lib/a2audio/rec.py
    	new file:   lib/a2audio/recanalizer.py
    	new file:   lib/a2audio/roiset.py
    	new file:   lib/a2audio/roizer.py
    	new file:   lib/a2pyutils/__init__.py
    	new file:   lib/a2pyutils/bmpio.py
    	new file:   lib/a2pyutils/colors.py
    	new file:   lib/a2pyutils/config.py
    	new file:   lib/a2pyutils/logger.py
    	new file:   lib/a2pyutils/news.py
    	new file:   lib/a2pyutils/palette.py
    	new file:   lib/a2pyutils/plotter.py
    	new file:   lib/a2pyutils/tempfilecache.py
    	new file:   lib/indices/__init__.py
    	new file:   lib/indices/indices.py
    	new file:   lib/soundscape/__init__.py
    	new file:   lib/soundscape/scidx.py
    	new file:   lib/soundscape/soundscape.py

---
+++ lib/a2pyutils/logger.py
@@ -0,0 +1,62 @@
+import os
+import tempfile
+import time
+
+class Logger:
+
+    def __init__(self,jobId,script,logFor='worker',logON = True):
+        self.logON = logON
+        self.also_print = False
+        if self.logON:
+            tempFolders = tempfile.gettempdir()
+            self.workingFolder = tempFolders+"/logs/job_"+str(jobId)
+            if not os.path.exists(self.workingFolder):
+                os.makedirs(self.workingFolder)
+                
+            lognametry=0;    
+            self.filePath = self.workingFolder+"/"+script+"_"+logFor+"_"+str(lognametry)+".log"
+            
+            while os.path.isfile(self.filePath):
+                lognametry =  lognametry + 1
+                self.filePath = self.workingFolder+"/"+script+"_"+logFor+"_"+str(lognametry)+".log"
+                
+            self.log_file_handle = open(self.filePath,'w')
+            self.write(script+' log file')
+            self.jobId = jobId
+            self.logFor = logFor
+    
+    def write(self,message):
+        if self.logON:
+            currTime = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+            if self.log_file_handle:
+                self.log_file_handle.write(currTime +':\t'+message+'\n')
+            else :
+                self.log_file_handle = open(self.filePath,'a')
+                self.log_file_handle.write(currTime +':\t'+message+'\n')
+            if self.log_file_handle:
+                self.log_file_handle.close()
+                self.log_file_handle = None
+            if self.also_print:
+                print "#LOG:" + currTime + ':\t'+message
+
+    def time_delta(self, message, start):
+        self.write("{} --- seconds --- {}".format(
+            message, time.time() - start
+        ))
+
+
+    def write_clean(self,message):
+        if self.logON:
+            self.log_file_handle.write(message)
+    
+    def close(self):
+        if self.logON:
+            if self.log_file_handle:
+                self.write('end of log')
+                self.log_file_handle.close()
+                
+    def __exit__(self, type, value, traceback):
+        if self.logON:
+            if self.log_file_handle:
+                self.write('end of log')
+                self.log_file_handle.close()
