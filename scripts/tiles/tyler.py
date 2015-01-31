c36c850abf0c19b51d9ee4d6b5032f837ca0b7b9
rafael-alvarez <rafael.alvarez.berrios@gmail.com>
2015-01-31 13:35:00 -0400

    Tyler complete but...
    Need to speed up spectrogram generation
    
     Changes to be committed:
    	modified:   lib/a2pyutils/config.py
    	modified:   scripts/tiles/tyler.py

--- scripts/tiles/tyler.py
+++ scripts/tiles/tyler.py
@@ -2,6 +2,8 @@
 
 import sys
 import os
+import multiprocessing
+from joblib import Parallel, delayed
 import MySQLdb
 import MySQLdb.cursors
 from a2pyutils import colors
@@ -38,7 +40,7 @@ def exit_error(msg, code=-1):
 if len(sys.argv) < 2:
     print USAGE
     sys.exit()
-    
+
 recording_id = int(sys.argv[1])
 palette_id = (
     (int(sys.argv[2]) if len(sys.argv) > 2 else 0) %
@@ -46,6 +48,7 @@ palette_id = (
 
 configuration = Config()
 config = configuration.data()
+num_cores = multiprocessing.cpu_count()
 
 try:
     db = MySQLdb.connect(
@@ -70,7 +73,7 @@ with closing(db.cursor()) as cursor:
 
 if not rec_data:
     exit_error("Recording#{} not found".format(recording_id))
-
+print("--- config %s seconds ---" % str(time.time() - start_time))
 conn = S3Connection(awsKeyId, awsKeySecret)
 try:
     bucket = conn.get_bucket(bucketName, validate=False)
@@ -80,11 +83,10 @@ except Exception, ex:
 rec_uri = rec_data['uri']
 rec_file = file_cache.fetch(rec_uri)
 if isinstance(rec_file, tempfilecache.CacheMiss):
-    print "Fetching {} from bucket".format(rec_uri)
     k = bucket.get_key(rec_uri, validate=False)
     k.get_contents_to_filename(rec_file.file)
     rec_file = rec_file.retry_get()
-
+print("--- recing %s seconds ---" % str(time.time() - start_time))
 rec_path = rec_file['path']
 encoding = 0
 channs = 0
@@ -98,9 +100,11 @@ original = []
 try:
     with closing(Sndfile(rec_path)) as f:
         encoding =  re.findall(r'\d+',f.encoding)
+        read_encoding = 16
         if len(encoding) > 0:
             encoding = int(encoding[0])
             if encoding not in [8,16,32,64]:
+                print encoding
                 if encoding < 8:
                     read_encoding = 8
                 elif encoding < 16:
@@ -109,9 +113,10 @@ try:
                     read_encoding = 32
                 else:
                     read_encoding = 64
+            else:
+                read_encoding = encoding
         else:
-            encoding = 16
-            read_encoding = 16
+            encoding = 'unknown. read as int16'
         file_format = f.file_format
         channs = f.channels
         samples = f.nframes
@@ -121,33 +126,80 @@ try:
         original = f.read_frames(f.nframes,dtype=numpy.dtype('int'+str(read_encoding)))
 except:
     exit_error('Error reading recording.')
-
+print("--- read rec %s seconds ---" % str(time.time() - start_time))
 try:       
     Pxx, freqs, bins = mlab.specgram(original, NFFT=512, Fs=sample_rate , noverlap=256)
+    Pxx[:,:] =  10. * numpy.log10( Pxx[:,:].clip(min=0.0000000001))
+    smin = min([min((Pxx[j])) for j in range(Pxx.shape[0])])
+    smax = max([max((Pxx[j])) for j in range(Pxx.shape[0])])
+    spectrogramMatrix = numpy.flipud(255*(1-((Pxx - smin)/(smax-smin))))
 except:
     exit_error('Error creating spectrogram.')
+print("--- spectro %s seconds ---" % str(time.time() - start_time))
+try:
+    num_of_cols = Pxx.shape[1]
+    num_of_tiles = 6
+    cols_per_tile = int(floor(num_of_cols/num_of_tiles))
+    last_tile_cols = num_of_cols- ( cols_per_tile * num_of_tiles)
+    px2sec = duration / num_of_cols 
+    max_freq = sample_rate / 2
+    px2hz  = max_freq / Pxx.shape[0]
+    spec_config = config[7]
+    tile_max_w = spec_config['tiles']['max_width']
+    tile_max_h = spec_config['tiles']['max_height']
+    tile_count_x = int(numpy.ceil(num_of_cols  * 1.0 / tile_max_w))
+    tile_count_y = int(numpy.ceil(Pxx.shape[0] * 1.0 / tile_max_h))
+except:
+    exit_error('Error calculating params.')
+print("--- params %s seconds ---" % str(time.time() - start_time))
+try:
+    tiles_x=[]
+    tiles_y=[]
+    tile_set=[]
+    
+    for x in range(tile_count_x):
+        tiles_x.append(x)
+    for y in range(tile_count_y):
+        tiles_y.append(y)
+    
+    total_y_tiles = len(tiles_y)
+    total_x_tiles = len(tiles_x)
+    
+    for tile_y in tiles_y:
+        tile_y0 = int(min( tile_y   *tile_max_h, Pxx.shape[0]))
+        tile_y1 = int(min((tile_y+1)*tile_max_h, Pxx.shape[0]))
+        tile_h  = tile_y1 - tile_y0
+        for tile_x in tiles_x:
+            tile_x0 = min( tile_x  *tile_max_w, num_of_cols)
+            tile_x1 = min((tile_x+1)*tile_max_w, num_of_cols)
+            tile_w  = tile_x1 - tile_x0
+            tile_set.append({
+                "j" : tile_x , "i" : tile_y ,
+                "s"  : (tile_x0 * px2sec),  "hz" : (max_freq - tile_y0 * px2hz),
+                "ds" : (tile_w * px2sec), "dhz" : (tile_h * px2hz),
+                "x" : tile_x0, "y" : tile_y0,
+                "x1" : tile_x1, "y1" : tile_y1,
+                "w" : tile_w , "h" : tile_h
+            })
+except:
+    exit_error('Error calculating tiles.')
+print("--- calc tiles %s seconds ---" % str(time.time() - start_time))   
+def saveTile(t,r_uri):
+    ext = '.flac'
+    if 'wav' in r_uri:
+        ext = '.wav'    
+    tile_key = r_uri.replace(ext, '.tile_'+str(t['j'])+'_'+str(t['i'])+'.png');
+    tile_file = file_cache.fetch(tile_key)
+    if isinstance(tile_file, tempfilecache.CacheMiss):
+        png.from_array(spectrogramMatrix[t['y']:(t['y1']-1),t['x']:(t['x1']-1)], 'L;8').save(tile_file.file)
 
-Pxx[:,:] =  10. * numpy.log10( Pxx[:,:].clip(min=0.0000000001))
-
-smin = min([min((Pxx[j])) for j in range(Pxx.shape[0])])
-smax = max([max((Pxx[j])) for j in range(Pxx.shape[0])])
-x = numpy.flipud(255*(1-((Pxx - smin)/(smax-smin))))
-
-num_of_cols = Pxx.shape[1]
-
-num_of_tiles = 6
-
-cols_per_tile = int(floor(num_of_cols/num_of_tiles))
-
-last_tile_cols = num_of_cols- ( cols_per_tile * num_of_tiles)
-
-j = 0
-for i in range(0,int(num_of_cols-last_tile_cols),cols_per_tile):
-    png.from_array(x[:,i:(i+cols_per_tile)], 'L;8').save('/home/rafa/tile_'+str(j)+".png")
-    j = j + 1
-
-png.from_array(x[:,(num_of_cols-last_tile_cols-1):(num_of_cols-1)], 'L;8').save('/home/rafa/tile_'+str(j)+".png")
-
+try:
+    Parallel(n_jobs=num_cores)(
+        delayed(saveTile)(tile ,rec_uri )
+        for tile in tile_set)
+except:
+    exit_error('Error saving tiles.')
+    
 print("--- %s seconds ---" % str(time.time() - start_time))
 
 
