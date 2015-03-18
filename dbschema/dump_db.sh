e83f6ca616f58fd4978f4251221fa3d52d30bf7c
Pedro A. Meléndez <lazy.chino@gmail.com>
2015-03-18 16:54:39 -0400

    added updated db file, change dumb_db.sh to receive db user as parameter

--- dbschema/dump_db.sh
+++ dbschema/dump_db.sh
@@ -1,9 +1,17 @@
 #! /bin/sh
+
+if [ -n "$1" ]
+then
+    user=$1
+else
+    user=$USER
+fi
+
 db=arbimon2
 prepop="job_types model_types permissions playlist_types project_news_types project_types roles role_permissions site_types soundscape_aggregation_types training_set_types user_account_support_type"
 echo "Dumping $db structure"
-mysqldump -p -d $db | sed 's/ AUTO_INCREMENT=[0-9][0-9]*//' > $db-structure.sql
+mysqldump -u $user -p -d $db | sed 's/ AUTO_INCREMENT=[0-9][0-9]*//' > $db-structure.sql
 echo "Dumping $db data"
-mysqldump -p -t $db > $db-data.sql
+mysqldump -u $user -p -t $db > $db-data.sql
 echo "Dumping $db prepopulated tables"
-mysqldump -p -t --skip-extended-insert --skip-add-locks $db $prepop > $db-prepop-data.sql
+mysqldump -u $user -p -t --skip-extended-insert --skip-add-locks $db $prepop > $db-prepop-data.sql
