5d68ecfadcfc997ce5d7454aed2918e8ca4e19d4
g-i-o- <aleph.omega@gmail.com>
2015-09-18 17:30:45 -0400

    added service to load google maps asynchronously and provide it through angular

---
+++ assets/js/a2gmaps.js
@@ -0,0 +1,43 @@
+angular.module('a2.googlemaps',[])
+.provider("a2GoogleMapsLoader", function(){
+    var defer;
+    var apiKey;
+    
+    function loadScript($window, $q) {
+        if(!defer){ 
+            return;
+        }
+        
+        var cb_name = 'gmcb';
+        while($window[cb_name]){
+            cb_name+='_';
+        }
+        
+        $window[cb_name] = function(){
+            defer.resolve($window.google);
+        };
+    
+        var script = $window.document.createElement('script');
+        var params = ['callback='+cb_name];
+        if(apiKey){ 
+            params.push('key='+apiKey);
+        }
+        script.src = $window.document.location.protocol + '//maps.googleapis.com/maps/api/js?'+params.join('&');
+        $window.document.body.appendChild(script);
+    }                
+    
+    return {
+        setAPIKey: function(key){
+            apiKey = key;
+        },        
+        $get: function($window, $q){
+            console.log("$get: function($window, $q){");
+            if(!defer){
+                defer = $q.defer();
+                loadScript($window, $q);
+            }
+            return defer.promise;
+        }
+    };
+})
+;
