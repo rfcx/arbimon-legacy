dd5627b9615e019b1caf204fae1cb8599e9146bf
g-i-o- <aleph.omega@gmail.com>
2015-11-05 15:52:18 -0400

    creating recordingFilterParameters directive and associated controller from filters code in RecsCtrl and templates

--- assets/js/audiodata/recordings.js
+++ assets/js/audiodata/recordings.js
@@ -6,59 +6,8 @@ angular.module('a2.audiodata.recordings', [
 ])
 .controller('RecsCtrl', function($scope, Project, a2Classi, $http, $modal, notify, a2UserPermit, $window) {
     
-    var readFilters = function() {
-        $scope.message = '';
-        var params = {};
-        
-        if($scope.params.range && $scope.params.range.from && $scope.params.range.to) {
-            params.range = $scope.params.range;
-            
-            params.range.from.setUTCHours(0);
-            params.range.from.setUTCMinutes(0);
-            params.range.to.setUTCHours(23);
-            params.range.to.setUTCMinutes(59);
-        }
-    
-        var mapValue = function(x) { return x.value; };
-        
-        if($scope.params.sites && $scope.params.sites.length) {
-            params.sites = $scope.params.sites.map(mapValue);
-        }
-        if($scope.params.hours && $scope.params.hours.length) {
-            params.hours = $scope.params.hours.map(mapValue);
-        }
-        if($scope.params.months && $scope.params.months.length) {
-            params.months = $scope.params.months.map(mapValue);
-        }
-        if($scope.params.years && $scope.params.years.length) {
-            params.years = $scope.params.years.map(mapValue);
-        }
-        if($scope.params.days && $scope.params.days.length) {
-            params.days = $scope.params.days.map(mapValue);
-        }
-        if($scope.params.validations && $scope.params.validations.length) {
-            params.validations = $scope.params.validations.map(function(soundClass) {
-                return soundClass.id;
-            });
-        }
-        if($scope.params.presence) {
-            params.presence = $scope.params.presence;
-        }
-        if($scope.params.classifications && $scope.params.classifications.length) {
-            params.classifications = $scope.params.classifications.map(function(classification) {
-                return classification.id;
-            });
-        }
-        if($scope.params.classification_results && $scope.params.classification_results.length) {
-            params.classification_results = $scope.params.classification_results.map(function(r) {
-                return r.flags;
-            });
-        }
-        
-        return params;
-    };
     var searchRecs = function(output) {
-        var params = readFilters();
+        var params = $scope.params;
         output = output || ['list'];
         params.output = output;
         params.limit = $scope.limitPerPage;
@@ -95,13 +44,225 @@ angular.module('a2.audiodata.recordings', [
         });
     };
     
+    $scope.sortRecs = function(sortKey, reverse) {
+        $scope.sortKey = sortKey;
+        $scope.reverse = reverse;
+        searchRecs();
+    };
+    $scope.applyFilters = function(filters) {
+        $scope.currentPage = 1;
+        $scope.params = filters;
+        searchRecs(['count', 'list']);
+    };
+    $scope.reloadList = function() {
+        searchRecs(['count', 'list']);
+    };
+    $scope.createPlaylist = function(filters) {
+        var listParams = filters;
+        
+        if(!Object.keys(listParams).length)
+            return;
+            
+        if(!a2UserPermit.can('manage playlists')) {
+            notify.log('You do not have permission to create playlists');
+            return;
+        }
+        
+        var modalInstance = $modal.open({
+            controller: 'SavePlaylistModalInstanceCtrl',
+            templateUrl: '/partials/audiodata/create-playlist.html',
+            resolve: {
+                listParams: function() {
+                    return listParams;
+                }
+            }
+        });
+        
+        modalInstance.result.then(function() {
+            notify.log('Playlist created');
+        });
+    };
+    
+    $scope.del = function() {
+        if(!a2UserPermit.can('manage project recordings')) {
+            notify.log('You do not have permission to delete recordings');
+            return;
+        }
+        
+        var recs = $scope.checked.filter(function(rec){ 
+                return !rec.imported; 
+            });
+            
+        if(!recs || !recs.length)
+            return notify.log('Recordings from imported sites can not be deleted');
+        
+        var recCount = {};
+        
+        for(var i = 0; i < recs.length; i++) {
+            recCount[recs[i].site] = recCount[recs[i].site] + 1 || 1;
+        }
+        
+        var sites = Object.keys(recCount).map(function(site) {
+            var s = recCount[site] > 1 ? 's' : '';
+            
+            return recCount[site] + ' recording'+s+' from "' + site + '"';
+        });
+        
+        
+        var msg = ["You are about to delete: "];
+        msg = msg.concat(sites);
+        msg.push("Are you sure??");
+        
+        var modalInstance = $modal.open({
+            templateUrl: '/partials/pop-up.html',
+            controller: function() {
+                this.messages = msg;
+                this.btnOk =  "Yes";
+                this.btnCancel =  "No, cancel";
+            },
+            controllerAs: 'popup'
+        });
+        
+        modalInstance.result.then(function() {
+            
+            var recIds = recs.map(function(rec) { return rec.id; });
+            
+            $http.post('/api/project/'+Project.getUrl()+'/recordings/delete', { recs: recs })
+                .success(function(data) {
+                    if(data.error)
+                        return notify.error(data.error);
+                    
+                    searchRecs(['count', 'list']);
+                    
+                    notify.log(data.msg);
+                });
+        });
+    };
+    
+    $scope.loading = true;
+    $scope.params = {};
+    $scope.loading = true;
+    $scope.currentPage  = 1;
+    $scope.limitPerPage = 10;
+
+    searchRecs(['count', 'date_range', 'list']);
+    
+    this.setCurrentPage = function(currentPage){
+        $scope.currentPage = currentPage;
+        searchRecs();
+    };
+    this.setLimitPerPage = function(limitPerPage){
+        $scope.limitPerPage = limitPerPage;
+        searchRecs();
+    };
+    
+})
+.controller('SavePlaylistModalInstanceCtrl', function($scope, $modalInstance, a2Playlists, listParams) {
+    $scope.savePlaylist = function(name) {
+        a2Playlists.create({
+            playlist_name: name,
+            params: listParams
+        },
+        function(data) {
+            if (data.error) {
+                $scope.errMess = data.error;
+            }
+            else {
+                $modalInstance.close();
+            }
+        });
+    };
+})
+.directive('recordingFilterParameters', function(){
+    return {
+        restrict:'E',
+        templateUrl:'/partials/audiodata/recording-filter-parameters.html',
+        scope:{
+            onApplyFilters : '&',
+            onCreatePlaylist : '&',
+        },
+        controller:'recordingFilterParametersController as controller',
+        requires:'^RecsCtrl', 
+        link: function(scope, element, attrs) {
+            var controller = scope.controller;
+            scope.applyFilters = function() {
+                scope.onApplyFilters({filters: controller.readFilters()});
+            };
+            scope.resetFilters = function() {
+                scope.params = {};
+                scope.onApplyFilters({filters: controller.readFilters()});
+            };
+            scope.createPlaylist = function() {
+                scope.onCreatePlaylist({filters: controller.readFilters()});
+            };
+            
+        }
+    };
+})
+.controller('recordingFilterParametersController', function($scope, Project, a2Classi, $http, $modal, notify, a2UserPermit, $window) {
+    
+    var readFilters = function() {
+        $scope.message = '';
+        var params = {};
+        
+        if($scope.params.range && $scope.params.range.from && $scope.params.range.to) {
+            params.range = $scope.params.range;
+            
+            params.range.from.setUTCHours(0);
+            params.range.from.setUTCMinutes(0);
+            params.range.to.setUTCHours(23);
+            params.range.to.setUTCMinutes(59);
+        }
+
+        var mapValue = function(x) { return x.value; };
+        
+        if($scope.params.sites && $scope.params.sites.length) {
+            params.sites = $scope.params.sites.map(mapValue);
+        }
+        if($scope.params.hours && $scope.params.hours.length) {
+            params.hours = $scope.params.hours.map(mapValue);
+        }
+        if($scope.params.months && $scope.params.months.length) {
+            params.months = $scope.params.months.map(mapValue);
+        }
+        if($scope.params.years && $scope.params.years.length) {
+            params.years = $scope.params.years.map(mapValue);
+        }
+        if($scope.params.days && $scope.params.days.length) {
+            params.days = $scope.params.days.map(mapValue);
+        }
+        if($scope.params.validations && $scope.params.validations.length) {
+            params.validations = $scope.params.validations.map(function(soundClass) {
+                return soundClass.id;
+            });
+        }
+        if($scope.params.presence) {
+            params.presence = $scope.params.presence;
+        }
+        if($scope.params.classifications && $scope.params.classifications.length) {
+            params.classifications = $scope.params.classifications.map(function(classification) {
+                return classification.id;
+            });
+        }
+        if($scope.params.classification_results && $scope.params.classification_results.length) {
+            params.classification_results = $scope.params.classification_results.map(function(r) {
+                return r.flags;
+            });
+        }
+        
+        console.log("readFilters", params);
+        return params;
+    };
+
+    this.readFilters = readFilters;
+
     var findObjectWith = function(arr, key, value) {
         var result = arr.filter(function(obj) {
             return obj[key] === value;
         });
         return result.length > 0 ? result[0] : null;
     };
-    
+
     var getAvalilableFilters = function(filters) {
         Project.getRecordingAvailability('---[1:31]', function(data) {
             
@@ -191,107 +352,8 @@ angular.module('a2.audiodata.recordings', [
             $scope.hours.sort(sort);
         });
     };
-    
-    $scope.sortRecs = function(sortKey, reverse) {
-        $scope.sortKey = sortKey;
-        $scope.reverse = reverse;
-        searchRecs();
-    };
-    $scope.applyFilters = function() {
-        $scope.currentPage = 1;
-        searchRecs(['count', 'list']);
-    };
-    $scope.resetFilters = function() {
-        $scope.currentPage = 1;
-        $scope.params = {};
-        searchRecs(['count', 'list']);
-    };
-    $scope.reloadList = function() {
-        searchRecs(['count', 'list']);
-    };
-    $scope.createPlaylist = function() {
-        var listParams = readFilters();
-        
-        if(!Object.keys(listParams).length)
-            return;
-            
-        if(!a2UserPermit.can('manage playlists')) {
-            notify.log('You do not have permission to create playlists');
-            return;
-        }
-        
-        var modalInstance = $modal.open({
-            controller: 'SavePlaylistModalInstanceCtrl',
-            templateUrl: '/partials/audiodata/create-playlist.html',
-            resolve: {
-                listParams: function() {
-                    return listParams;
-                }
-            }
-        });
-        
-        modalInstance.result.then(function() {
-            notify.log('Playlist created');
-        });
-    };
-    
-    $scope.del = function() {
-        if(!a2UserPermit.can('manage project recordings')) {
-            notify.log('You do not have permission to delete recordings');
-            return;
-        }
-        
-        var recs = $scope.checked.filter(function(rec){ 
-                return !rec.imported; 
-            });
-            
-        if(!recs || !recs.length)
-            return notify.log('Recordings from imported sites can not be deleted');
-        
-        var recCount = {};
-        
-        for(var i = 0; i < recs.length; i++) {
-            recCount[recs[i].site] = recCount[recs[i].site] + 1 || 1;
-        }
-        
-        var sites = Object.keys(recCount).map(function(site) {
-            var s = recCount[site] > 1 ? 's' : '';
-            
-            return recCount[site] + ' recording'+s+' from "' + site + '"';
-        });
-        
-        
-        var msg = ["You are about to delete: "];
-        msg = msg.concat(sites);
-        msg.push("Are you sure??");
-        
-        var modalInstance = $modal.open({
-            templateUrl: '/partials/pop-up.html',
-            controller: function() {
-                this.messages = msg;
-                this.btnOk =  "Yes";
-                this.btnCancel =  "No, cancel";
-            },
-            controllerAs: 'popup'
-        });
-        
-        modalInstance.result.then(function() {
-            
-            var recIds = recs.map(function(rec) { return rec.id; });
-            
-            $http.post('/api/project/'+Project.getUrl()+'/recordings/delete', { recs: recs })
-                .success(function(data) {
-                    if(data.error)
-                        return notify.error(data.error);
-                    
-                    searchRecs(['count', 'list']);
-                    
-                    notify.log(data.msg);
-                });
-        });
-    };
-    
-    $scope.loading = true;
+
+
     $scope.params = {};
     $scope.classes = [];
     $scope.classifications = [];
@@ -301,10 +363,9 @@ angular.module('a2.audiodata.recordings', [
     $scope.months = [];
     $scope.days = [];
     $scope.hours = [];
-    $scope.loading = true;
     $scope.currentPage  = 1;
     $scope.limitPerPage = 10;
-    
+
     var classification_results={
         model_only : [
             {caption:'<i class="fa a2-absent"></i> Absent', tooltip:'Absent' , flags:{model:0}, equiv:{model_th:[0,2]}},
@@ -317,12 +378,12 @@ angular.module('a2.audiodata.recordings', [
             {caption:'Model: <i class="fa a2-present"></i>, Th: <i class="fa a2-present"></i>'        , tooltip:'Model: present, Theshold: present', flags:{model:1, th:1}, equiv:{model_only:[1]}}
         ]
     };
-    
+
     this.data = {
         classifications:[],
         classification_results: classification_results.model_only
     };
-    
+
     Project.getClasses(
         {
             validations: true,
@@ -340,7 +401,7 @@ angular.module('a2.audiodata.recordings', [
             return c;
         });
     }).bind(this));
-    
+
     this.computeClassificationResults = function(){
         var haveThreshold = $scope.params.classifications && $scope.params.classifications.reduce(function(a, b){
             return a || !!b.threshold;
@@ -365,34 +426,7 @@ angular.module('a2.audiodata.recordings', [
             }, {list:[], index:{}}).list;
         }
     };
-    
+
     getAvalilableFilters({});
-    searchRecs(['count', 'date_range', 'list']);
-    
-    this.setCurrentPage = function(currentPage){
-        $scope.currentPage = currentPage;
-        searchRecs();
-    };
-    this.setLimitPerPage = function(limitPerPage){
-        $scope.limitPerPage = limitPerPage;
-        searchRecs();
-    };
-    
-})
-.controller('SavePlaylistModalInstanceCtrl', function($scope, $modalInstance, a2Playlists, listParams) {
-    $scope.savePlaylist = function(name) {
-        a2Playlists.create({
-            playlist_name: name,
-            params: listParams
-        },
-        function(data) {
-            if (data.error) {
-                $scope.errMess = data.error;
-            }
-            else {
-                $modalInstance.close();
-            }
-        });
-    };
 })
 ;
