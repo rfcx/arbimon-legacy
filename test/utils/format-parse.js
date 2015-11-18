edcd25fe87adfb897160da6c1c42da574be57748
g-i-o- <aleph.omega@gmail.com>
2015-11-18 11:55:06 -0400

    Modified Arbimon time format to allow for seconds. Also updated related tests

--- test/utils/format-parse.js
+++ test/utils/format-parse.js
@@ -7,24 +7,43 @@ var root = '../../';
 var formatParse = require(root + 'utils/format-parse');
 
 describe('format-parse', function(){
-    it('should return object when the filename complies Wildlife time format', function(){
-        var result = formatParse('Wildlife','TEST_SM3_ 20140530_0630000.mp3');
-        
-        result.should.deep.equal({
-            filename: 'TEST_SM3_ 20140530_0630000',
-            datetime: new Date(2014, 04, 30, 06, 30),
-            filetype: '.mp3'
+    describe('Wildlife format', function(){
+        it('should parse datetime correctly.', function(){
+            var result = formatParse('Wildlife','TEST_SM3_ 20140530_0630000.mp3');
+            
+            result.should.deep.equal({
+                filename: 'TEST_SM3_ 20140530_0630000',
+                datetime: new Date(2014, 4, 30, 6, 30),
+                filetype: '.mp3'
+            });
         });
     });
     
-    it('should return object when the filename complies Arbimon time format', function(){
-        var result = formatParse('Arbimon','default-2014-08-25_17-30.flac');
+    describe('Arbimon format', function(){
         
-        result.should.deep.equal({
-            filename: 'default-2014-08-25_17-30',
-            datetime: new Date(2014, 07, 25, 17, 30),
-            filetype: '.flac' 
+        it('should parse datetime correctly.', function(){
+            var result = formatParse('Arbimon','default-2014-08-25_17-30.flac');
+            
+            result.should.deep.equal({
+                filename: 'default-2014-08-25_17-30',
+                datetime: new Date(2014, 7, 25, 17, 30),
+                filetype: '.flac' 
+            });
         });
+        
+        it('should parse datetime correctly when format has seconds.', function(){
+            formatParse('Arbimon','default-2014-08-25_17-30-45.flac').should.deep.equal({
+                filename: 'default-2014-08-25_17-30-45',
+                datetime: new Date(2014, 7, 25, 17, 30, 45),
+                filetype: '.flac' 
+            });
+            formatParse('Arbimon','test-2015-03-16_15-44-55.flac').should.deep.equal({
+                filename: 'test-2015-03-16_15-44-55',
+                datetime: new Date(2015, 2, 16, 15, 44, 55),
+                filetype: '.flac' 
+            });            
+        });
+    
     });
     
     it('should throw error when format name is invalid', function() {
@@ -48,5 +67,4 @@ describe('format-parse', function(){
         
     });
     
-    console.log();
 });
--- utils/format-parse.js
+++ utils/format-parse.js
@@ -17,7 +17,7 @@
 var formatParse = function(formatName, filename) {
     var formats = {
         Wildlife: /(.*(\d{4})(\d{2})(\d{2})[_|\$](\d{2})(\d{2})\d{2}.*)(\.\w+)$/,
-        Arbimon: /(.*(\d{4})-(\d{2})-(\d{2})_(\d{2})-(\d{2}).*)(\.\w+)$/
+        Arbimon: /(.*(\d{4})-(\d{2})-(\d{2})_(\d{2})-(\d{2})(-(\d{2}))?.*)(\.\w+)$/
     };
     
     
@@ -31,6 +31,15 @@ var formatParse = function(formatName, filename) {
         throw new Error('invalid_filename: "' + filename + '"');
     }
     
+    if(formatName == 'Arbimon'){
+        return {
+            filename: results[1],
+            datetime: results[7] ? new Date(results[2], (results[3]-1), results[4], results[5], results[6], results[8]):
+                                   new Date(results[2], (results[3]-1), results[4], results[5], results[6]),
+            filetype: results[9]
+        };
+    }
+    
     return {
         filename: results[1],
         datetime: new Date(results[2], (results[3]-1), results[4], results[5], results[6]),
@@ -38,5 +47,6 @@ var formatParse = function(formatName, filename) {
     };
 
 };
+formatParse.formats = {}
 
 module.exports = formatParse;
