402e04c5c7b606e28606b7103e0c03959307a5ab
g-i-o- <aleph.omega@gmail.com>
2015-11-18 17:49:39 -0400

    added getForType to a2Tags, also corrected bug where bbox was not sent to the a2API

--- assets/js/a2services/tags-service.js
+++ assets/js/a2services/tags-service.js
@@ -1,7 +1,10 @@
 angular.module('a2.srv.tags', ['a2.srv.api'])
 .factory('a2Tags', function($q, a2APIService) {
+    function isTaggableType(type){
+        return /^recording$/.test('' + type);
+    }
     function isTaggableResource(obj){
-        return obj && /^recording$/.test(obj.type) && obj.id;
+        return obj && isTaggableType(obj.type) && obj.id;
     }
     return {
         getFor : function(visobject){
@@ -10,6 +13,13 @@ angular.module('a2.srv.tags', ['a2.srv.api'])
             } else {
                 return $q.resolve([]);
             }
+        },
+        getForType : function(resourceType){
+            if(isTaggableType(resourceType)){
+                return a2APIService.get('/tags/' + resourceType);
+            } else {
+                return $q.resolve([]);
+            }            
         },
         deleteFor : function(visobject, tagId){
             if(isTaggableResource(visobject)){
@@ -25,6 +35,12 @@ angular.module('a2.srv.tags', ['a2.srv.api'])
                     data.id = tag.tag_id;
                 } else {
                     data.text = tag.tag;
+                }
+                if(tag.t0 !== undefined){
+                    data.t0 = tag.t0;
+                    data.f0 = tag.f0;
+                    data.t1 = tag.t1;
+                    data.f1 = tag.f1;
                 }
                 return a2APIService.put('/tags/' + visobject.type + '/' + (visobject.id|0), data);
             } else {
