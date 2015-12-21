d63bf896eabf674d820c12f5d5273b8c753f8839
g-i-o- <aleph.omega@gmail.com>
2015-12-21 12:41:56 -0400

    removing console logs

--- utils/job-scheduler.js
+++ utils/job-scheduler.js
@@ -26,23 +26,17 @@ JobScheduler.prototype = {
     /** Runs the scheduler if not running, otherwise it is a no-op.
      */
     run: function(){
-console.log("run :: run: function(){");
         if(this.running){
-console.log("run :: if(this.running){");
             return;
         }
         
         this.running = true;
         return this.oneRunIteration().catch((function(error){
-console.log("run :: return this.oneRunIteration().catch((function(error){");
             return this.onError(error);
         }).bind(this)).finally(function(){
-console.log("run :: }).bind(this)).finally(function(){");
             this.running=false;
         }).then((function(){
-console.log("run :: }).then((function(){");
             if(this.drain){
-console.log("run :: if(this.drain){");
                 return this.drain(this);
             }
         }).bind(this));
@@ -52,26 +46,18 @@ console.log("run :: if(this.drain){");
      */
     oneRunIteration: function(){
         return q().then((function(){
-console.log("oneRunIteration :: return q().then((function(){");
             if(!this.queue.length){
-console.log("oneRunIteration :: if(!this.queue.length){");
                 return this._fillQueue();
             }
         }).bind(this)).then((function(){
-console.log("oneRunIteration :: }).bind(this)).then((function(){");
             if(this.queue.length){
-console.log("oneRunIteration :: if(this.queue.length){");
                 return q(this.queue.shift()).then((function(jobItem){
-console.log("oneRunIteration :: return q(this.queue.shift()).then((function(jobItem){");
                     if(jobItem && this.process){
-console.log("oneRunIteration :: if(jobItem && this.process){");
                         return this.process(jobItem);
                     }
                 }).bind(this)).catch((function(error){
-console.log("oneRunIteration :: }).bind(this)).catch((function(error){");
                     return this.onError(error);
                 }).bind(this)).then((function(){
-console.log("oneRunIteration :: }).bind(this)).then((function(){");
                     return this.oneRunIteration();
                 }).bind(this));
             }            
