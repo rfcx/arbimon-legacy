616d2be39f72be1fe46e10d2e61d1caa7606feac
g-i-o- <aleph.omega@gmail.com>
2016-02-24 11:35:22 -0400

    listAll now has options argument. Also, hash now uses base64 encoding for smaller size.

--- model/activation-codes.js
+++ model/activation-codes.js
@@ -10,10 +10,41 @@ var db = require('../utils/dbpool');
 var sha256 = require('../utils/sha256');
 
 var ActivationCodes = {
-    listAll: function(callback) {
+    /** Return list of ActivationCodes.
+     * @param {Object} options - options.
+     * @param {Object} options.hash - match by hash.
+     * @param {Object} options.consumer - match usable by user.
+     * @param {Object} options.project - match usable for project.
+     */
+    listAll: function(options) {
+        options = options || {};
+        var where = [], data = [];
+        
+        if(options.hash){
+            where.push('hash = ?');
+            data.push(options.hash);
+        }
+        
+        if(options.hasOwnProperty('consumed')){
+            where.push('consumed = ?');
+            data.push(options.consumed);
+        }
+        
+        if(options.consumer){
+            where.push(options.strict ? 'consumer = ?' : 'consumer = ? OR consumer IS NULL');
+            data.push(options.consumer);
+        }
+        
+        if(options.project){
+            where.push(options.strict ? 'project = ?' : 'project = ? OR project IS NULL');
+            data.push(options.project);
+        }
+        
         return db.query(
             "SELECT activation_code_id as id, hash, created, creator, payload, consumed, consumer, project\n"+
-            "FROM activation_codes"
+            "FROM activation_codes" + 
+            (where.length ? "\nWHERE (" + where.join(")\n AND (") + ")" : ""),
+            data
         ).then(function(data){
             return data.map(function(item){
                 item.payload = JSON.parse(item.payload);
@@ -21,23 +52,27 @@ var ActivationCodes = {
             });
         });
     },
-    createCode: function(creator, data){
+    makeHash: function(data){
         var salt = Math.ceil(new Date().getTime() / 1000).toString();
-        var hash = sha256(salt + JSON.stringify([
+        return sha256(salt + JSON.stringify([
             (data.processing | 0),
             (data.recordings | 0),
             (data.user | 0),
             (data.project | 0),
-        ]) + Math.random());
+        ]) + Math.random(), 'base64').replace(/\//g,'-').replace(/=+$/,'');
+    },
+    createCode: function(creator, data){
+        var hash = this.makeHash(data);
         return db.query(
             "INSERT INTO activation_codes(hash, created, creator, payload, consumed, consumer, project)\n"+
             "VALUES (?, NOW(), ?, ?, 0, ?, ?)", [
                 hash, creator.id, JSON.stringify({
                     processing : (data.processing | 0),
                     recordings : (data.recordings | 0),
+                    duration : (data.duration | 0),
                 }),
-                data.user | 0,
-                data.project | 0,
+                (data.user | 0) || null,
+                (data.project | 0) || null,
             ]
         );
     },
