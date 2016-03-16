bc83a388244a520f65b729e3f17f47161913864f
g-i-o- <aleph.omega@gmail.com>
2016-03-16 17:13:04 -0400

    Added routes: /at/project /at/project/sites /at/recording/check-exists

--- routes/access-token.js
+++ routes/access-token.js
@@ -38,4 +38,43 @@ router.post('/at/projectlist', function(req, res, next){
     }).catch(next);
 });
 
+
+router.post('/at/project', function(req, res, next){
+    res.type('json');
+    model.AccessTokens.verifyTokenAccess(req.body.token, null, {allowEmptyScope:true}).then(function(resolvedToken){
+        return model.users.hasProjectAccess(resolvedToken.user, req.body.project, {required:true});
+    }).then(function(){
+        return model.projects.find({id: req.body.project, basicInfo:true}).get(0);
+    }).then(function(project){
+        res.json(project);
+    }).catch(next);
+});
+
+
+router.post('/at/project/sites', function(req, res, next){
+    res.type('json');
+    model.AccessTokens.verifyTokenAccess(req.body.token, null, {allowEmptyScope:true}).then(function(resolvedToken){
+        return model.users.hasProjectAccess(resolvedToken.user, req.body.project, {required:true});
+    }).then(function(){
+        return model.projects.getProjectSites(req.body.project);
+    }).then(function(sites){
+        res.json(sites);
+    }).catch(next);
+});
+
+router.post('/at/recording/check-exists', function(req, res, next) {
+    res.type('json');
+    model.AccessTokens.verifyTokenAccess(req.body.token, null, {allowEmptyScope:true}).then(function(resolvedToken){
+        return model.users.hasProjectAccess(resolvedToken.user, req.body.project, {required:true});
+    }).then(function(){
+        return model.recordings.exists({
+            site_id: req.body.site,
+            filename: req.body.filename,
+        });
+    }).then(function(result){
+        res.json({exists: result});
+    }).catch(next);
+});
+
+
 module.exports = router;
