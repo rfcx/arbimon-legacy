bb0e774f8c8fa2496f266413a390d73895d6599e
g-i-o- <aleph.omega@gmail.com>
2016-03-17 13:11:24 -0400

    added doc comments

--- model/access-tokens.js
+++ model/access-tokens.js
@@ -23,8 +23,10 @@ var AccessTokens = {
      * @params options.username - username used in the oauth autorization.
      * @params options.password - password used in the oauth autorization.
      * @params options.project  - project associated to the requested token.
-     * @params options.site     - site associated to the given token.
+     * @params options.site     - site associated to the requested token.
      * @params options.scope    - the scope being requested.
+     *
+     * @returns {Promise} resolving to an access token, if the challenge was successful.
      */
     requestAccessToken: function(options){
         options = options || {};
@@ -33,6 +35,17 @@ var AccessTokens = {
         });
     },
     
+    /** Constructs an access token, given a scope and authentication information.
+     *
+     * @params auth - token authentication data
+     * @params auth.user - id of the user represented by the token
+     * @params auth.project  - project associated to the token.
+     * @params auth.site     - site associated to the token.
+     * @params auth.mustBeShortLived     - indicates that the token should expire promptly.
+     * @params scope    - the scope associated to the token.
+     *
+     * @returns {String} access token with the given data.
+     */
     constructAccessToken: function(auth, scope){
         var payload = {a2at:true, scope: scope};
         if(auth.token){
@@ -49,7 +62,20 @@ var AccessTokens = {
         return jsonwebtoken.sign(payload, config('tokens').secret, config('tokens').options);
     },
     
-    challengeAccessTokenRequest: function(options){
+    /** Challenges a given token request.
+     *
+     * @params options - options
+     * @params options.token    - A parent token authenticating the challenge.
+     * @params options.username - username of the user authenticating the challenge.
+     * @params options.password - password of the user authenticating the challenge.
+     * @params options.project  - project associated to the requested token.
+     * @params options.site     - site associated to the requested token.
+     * @params options.scope    - the scope being requested.
+     *
+     * @returns {Promise} resolving to a challenge result containing the user or token that passed the challenge,
+     *                    or rejecting with an error if the challenge failed.
+     */
+     challengeAccessTokenRequest: function(options){
         options = options || {};
         var challengeResult;
         return (
@@ -117,6 +143,13 @@ var AccessTokens = {
         });
     },
     
+    /** Decodes a given token and verifies that it is valid.
+     * @param {String} token - the token to decode/verify.
+     * @param {Object} options - options object.
+     * @param {Boolean} options.resolveIds - whether to resolve the ids in the token or not (default false).
+     *
+     * @returns {Promise} resolving to decoded token, or rejected if the token is not valid or has expired.
+     */
     resolveAccessToken: function(token, options){
         console.log("resolve :: ", token);
         options = options || {};
@@ -150,6 +183,14 @@ var AccessTokens = {
         });
     },
 
+    /** Resolves a given token and verifies that it has the access being queried.
+     * @param {String} token - the token to decode/verify.
+     * @param {String} scope - the scope against which to compare the token.
+     * @param {Object} options - options object, allowed options include those in resolveAccessToken().
+     * @param {Boolean} options.allowEmptyScope - wether an empty scope is allowed in the query or not.
+     *
+     * @returns {Promise} resolving to resolved token, if the token is valid and includes the required scope.
+     */
     verifyTokenAccess: function(token, scope, options){
         options = options || {};
         scope = options.allowEmptyScope && !scope ? [] : scope.split(',');
