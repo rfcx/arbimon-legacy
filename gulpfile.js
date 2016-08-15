e886d77917c489e59a3f8ef8b75041e7137460e0
g-i-o- <aleph.omega@gmail.com>
2016-08-15 11:57:06 -0400

    adding clean and dependencies tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var concat = require('gulp-concat');
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var uglify = require('gulp-uglify');
 var livereload = require('gulp-livereload');
 var less = require('gulp-less');
@@ -9,12 +10,46 @@ var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 var templateCache = require('gulp-angular-templatecache');
 var pump = require('pump');
+var path = require('path');
+var rimraf = require('rimraf');
+var q = require('q');
 var childProcess = require('child_process');
 
 var app={
     server: {
         bin: './bin/www',
         src: './app/**/*',
+    },
+    dependencies: {
+        src: './bower_components',
+        dest:'public/assets',
+        deps:{
+            angular              :{'angular.min.*': '' },
+            'angular-bootstrap'  :{'ui-bootstrap-tpls.min.js': '' },
+            'angular-file-upload':{'dist/angular-file-upload.min.*': '', },
+            'angular-rangeslider':{'angular.rangeSlider.*': '../angular-range-slider/', },
+            'angular-sanitize'   :{'angular-sanitize.min.*': '', },
+            'angular-ui-router'  :{'release/angular-ui-router.min.js': '' },
+            'angular-ui-select'  :{'dist/select.min.*': '', },
+            angularytics         :{'dist/angularytics.min.js': '', },
+            bootstrap            :{'dist/**' : '', },
+            c3                   :{'*': '', },
+            d3                   :{'d3.min.js': '', },
+            'esri-leaflet'       :{'dist/**': '', },
+            'font-awesome'       :{'css/*' : 'css/', 'fonts/*' : 'fonts/', },
+            'humane-js'          :{'humane.min.js': '', },
+            jquery               :{'dist/jquery.min.*': '', },
+            'jquery-ui'          :{'jquery-ui.min.js': '../jquery/', },
+            leaflet              :{'dist/**': '', },
+            moment               :{'min/moment.min.js': '', },
+            'ng-csv'             :{'build/ng-csv.min.js': '', },
+            'ng-table'           :{'ng-table.*': '', },
+            'qr-js'              :{'qr.min.*': '', },
+            'roboto-fontface'    :{'css/*': 'css/', 'fonts/*' : 'fonts/', },
+            'selectize'          :{'dist/css/selectize.default.css': '../angular-ui-select/', },
+            'ui-router-extras'   :{'release/ct-ui-router-extras.min.js': '' },
+            zxcvbn               :{'dist/zxcvbn.*': '', },
+        },
     },
     code:{
         name: 'arbimon2.js',
@@ -36,9 +71,10 @@ var app={
         ],
         dest: './public/assets/css/',
     },
+    clean : './public/assets',
 };
 
-gulp.task('default', ['app:code', 'app:templates', 'app:less']);
+gulp.task('default', ['app:code', 'app:templates', 'app:less', 'app:dependencies']);
 
 gulp.task('watch', ['livereload', 'app:server', 'app:watch']);
 
@@ -98,6 +134,38 @@ gulp.task('app:less', function(done){
     ], done);
 });
 
+gulp.task('app:clean', function(done){
+    rimraf(app.clean, done);
+});
+
+gulp.task('app:dependencies', function(done){
+    function copy_deps(src, dest, dependencies){
+        return q.all(Object.keys(dependencies).map(function(depkey){
+            var dep = dependencies[depkey];
+            var dsrc = path.join(src, depkey);
+            var ddst = path.join(dest, depkey);
+            
+            if('object' == typeof dep){
+                return copy_deps(dsrc, ddst,  dep);
+            } else if('string' == typeof dep){
+                ddst = path.join(dest, dep);
+                return q.nfcall(pump, [
+                    gulp.src(dsrc),
+                    gulp.dest(ddst),
+                ]).then(function(){
+                    gutil.log("Copied " + dsrc + " -> " + ddst);
+                });
+            }
+        }));
+    }
+        
+    copy_deps(
+        app.dependencies.src,
+        app.dependencies.dest,
+        app.dependencies.deps
+    ).nodeify(done);
+});
+
 var server_process;
 gulp.task('app:server', function(){
     if(server_process){
@@ -107,7 +175,7 @@ gulp.task('app:server', function(){
         stdio: 'inherit'
     });
     server_process.on('close', function(code){
-        gulp.log("Server exited with code " + code);
+        gutil.log("Server exited with code " + code);
     });
 });
 
