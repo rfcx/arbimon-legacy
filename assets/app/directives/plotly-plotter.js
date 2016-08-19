c6d8f649b606ac80f2f6d0d88a04bf616f5797d9
g-i-o- <aleph.omega@gmail.com>
2016-08-19 09:31:43 -0400

    plotly-plotter no has layout and data scope bindings, along with config defaults

--- assets/app/directives/plotly-plotter.js
+++ assets/app/directives/plotly-plotter.js
@@ -2,32 +2,37 @@ angular.module('a2.directive.plotly-plotter', [
     'a2.directive.on-resize'
 ])
 .directive('plotlyPlotter', function($window, a2OnResizeService) {
+    var config = {showLink:false, sendData:false, displaylogo:false, displayModeBar:true};
     return {
         restrict: 'E',
         scope: {
-            // columns: '='
+            layout: '=?',
+            data: '=?'
         },
         link: function(scope, element, attrs) {
-            var x = [];
-            var y = [];
-            for (var i = 0; i < 500; i ++) {
-            	x[i] = Math.random();
-            	y[i] = Math.random() + 1;
-            }
 
-            var data = [{
-                x: x,
-                y: y,
-                type: 'histogram2d'
-            }];
-            
-            Plotly.newPlot(element[0], data);
-            console.log("Plotly.newPlot element::", element[0]);
+            Plotly.newPlot(element[0], scope.data || [], scope.layout || {}, config);
 
             var resizeWatcher = a2OnResizeService.newWatcher(element, function(newSize){
-                console.log("Plotly.newPlot resize::", newSize);
                 Plotly.relayout(element[0], newSize);
             });
+            
+            if(attrs.layout){
+                scope.$watch('layout', function(layout, old){
+                    if(layout && layout != old){
+                        Plotly.relayout(element[0], layout);
+                    }
+                });
+            }
+            
+            if(attrs.data){
+                scope.$watch('data', function(data, old){
+                    if(data && data != old){
+                        element[0].data = data;
+                        Plotly.redraw(element[0]);
+                    }
+                });
+            }
 
             scope.$on('$destroy', function(){
                 resizeWatcher.destroy();
