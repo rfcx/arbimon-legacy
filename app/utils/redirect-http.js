bdf55911812b95b3e4ff06cbe6b02f25e97bbd07
g-i-o- <aleph.omega@gmail.com>
2018-02-06 19:50:01 -0400

    fixing location

--- app/utils/redirect-http.js
+++ app/utils/redirect-http.js
@@ -1,6 +1,10 @@
 var app = require('express')();
 
+var acmeChallenge = require('../routes/acme-challenge');
+
 app.enable('trust proxy');
+
+app.use('/', acmeChallenge);
 
 app.use(function(req, res, next){
     return res.redirect(301, 'https://' + req.hostname + req.originalUrl);
