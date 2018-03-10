b46bab206f4203fc42e4bf2a664f2d561cb0a1cf
Ian Flores <iflores.siaca@gmail.com>
2018-03-10 13:40:11 -0400

    Audimoth File Format Addition

--- app/utils/format-parse.js
+++ app/utils/format-parse.js
@@ -4,10 +4,10 @@
  * @param {String} formatName - Name that indentifies the format in use on the filename, can be an array of formats, or "any".
  *                              "any" causes it to use the first matching format.
  * @param {String} filename - filename to parse
- * 
+ *
  * @example
  * var formatParse = require('util/format-parse');
- * 
+ *
  * ffi = formatParse('Arbimon', 'stereo_wav-2014-07-14_08-20.wav');
  * @returns ffi - file format info [Object]<br>
  *  - ffi.filename [String] - filename without file extension<br>
@@ -19,20 +19,21 @@ var formatParse = function(formatName, filename) {
     var formats = {
         Cornell : /(.*(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})\d{2}.*)(\.\w+)$/,
         Wildlife: /(.*(\d{4})(\d{2})(\d{2})[_|\$](\d{2})(\d{2})\d{2}.*)(\.\w+)$/,
-        Arbimon: /(.*(\d{4})-(\d{2})-(\d{2})_(\d{2})-(\d{2})(-(\d{2}))?.*)(\.\w+)$/
+        Arbimon: /(.*(\d{4})-(\d{2})-(\d{2})_(\d{2})-(\d{2})(-(\d{2}))?.*)(\.\w+)$/,
+        AudioMoth: /([0-9A-F]{8})(\.\w+)$/
     };
     var parsed, errors=[];
-    
+
     if(formatName == 'any'){
         formatName = Object.keys(formats);
     }
-    
+
     var formatNames = formatName instanceof Array ? formatName.slice() : [formatName];
-    
+
     while(!parsed && formatNames.length){
         formatName = formatNames.shift();
         var results = formats[formatName] && formats[formatName].exec(filename);
-        
+
         if(results) {
             if(formatName == 'Arbimon'){
                 parsed = {
@@ -40,6 +41,12 @@ var formatParse = function(formatName, filename) {
                     datetime: results[7] ? new Date(results[2], (results[3]-1), results[4], results[5], results[6], results[8]):
                     new Date(results[2], (results[3]-1), results[4], results[5], results[6]),
                     filetype: results[9]
+                };
+            } else if (formatName == 'AudioMoth')
+                parsed = {
+                    filename: results[1],
+                    datetime: new Date(parseInt(results[1], 16)*1000),
+                    filetype: results[2]
                 };
             } else {
                 parsed = {
@@ -50,11 +57,11 @@ var formatParse = function(formatName, filename) {
             }
         }
     }
-    
+
     if(!parsed){
         throw new Error('invalid_filename: "' + filename + '"');
     }
-    
+
     return parsed;
 
 };
