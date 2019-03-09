a5524f0b6409413e36fae3363d147823079533d8
g-i-o- <aleph.omega@gmail.com>
2019-03-09 12:06:44 -0400

    adding templates service

---
+++ assets/app/a2services/templates-service.js
@@ -0,0 +1,37 @@
+angular.module('a2.srv.templates', ['a2.srv.project'])
+.factory('a2Templates', function(Project, $http) {
+    return {
+        getList: function(callback) {
+            var projectName = Project.getUrl();
+            return $http.get('/api/project/'+projectName+'/templates').then(function(response) {
+                return response.data;
+            });
+        },
+
+        add: function(template_data, callback) {
+            var projectName = Project.getUrl();
+            return $http.post('/api/project/'+projectName+'/templates/add', template_data).then(function(response) {
+                return response.data;
+            });
+        },
+
+        // edit: function(templateId, template_data) {
+        //     var projectName = Project.getUrl();
+        //     return $http.post('/api/project/'+projectName+'/templates/edit/'+templateId, template_data);
+        // },
+        //
+        // delete: function(templateId) {
+        //     var projectName = Project.getUrl();
+        //     return $http.post('/api/project/'+projectName+'/templates/remove/'+templateId);
+        // },
+
+        getImage: function(templateId, callback) {
+            var projectName = Project.getUrl();
+            return $http.get('/api/project/'+projectName+'/templates/data/'+templateId+'/image').then(function(response) {
+                return response.data;
+            });
+        },
+
+    };
+})
+;
