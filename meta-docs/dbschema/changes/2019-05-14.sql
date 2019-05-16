07231651f64a93355e190f8978b41b4ec1d64926
gio <aleph.omega@gmail.com>
2019-05-16 03:52:35 -0400

    fixing sqls

--- meta-docs/dbschema/changes/2019-05-14.sql
+++ meta-docs/dbschema/changes/2019-05-14.sql
@@ -4,7 +4,7 @@ ADD COLUMN `consensus_validated` TINYINT(1) NULL DEFAULT NULL AFTER `validated`;
 
 CREATE TABLE `arbimon2`.`pattern_matching_validations` (
   `validation_id` INT NOT NULL AUTO_INCREMENT,
-  `pattern_matching_id` INT NOT NULL,
+  `pattern_matching_roi_id` INT NOT NULL,
   `user_id` INT UNSIGNED NOT NULL,
   `timestamp` TIMESTAMP NOT NULL,
   `validated` INT NOT NULL,
@@ -13,8 +13,8 @@ CREATE TABLE `arbimon2`.`pattern_matching_validations` (
 
   ALTER TABLE `arbimon2`.`pattern_matching_validations`
   ADD CONSTRAINT `fk_pattern_matching_validations_1`
-    FOREIGN KEY (`pattern_matching_id`)
-    REFERENCES `arbimon2`.`pattern_matchings` (`pattern_matching_id`)
+    FOREIGN KEY (`pattern_matching_roi_id`)
+    REFERENCES `arbimon2`.`pattern_matching_rois` (`pattern_matching_roi_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_pattern_matching_validations_2`
--- meta-docs/dbschema/changes/2019-05-15.sql
+++ meta-docs/dbschema/changes/2019-05-15.sql
@@ -2,8 +2,8 @@ CREATE TABLE `arbimon2`.`pattern_matching_user_statistics` (
   `user_statistics_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` INT UNSIGNED NOT NULL,
   `project_id` INT UNSIGNED NOT NULL,
-  `species_id` INT UNSIGNED NOT NULL,
-  `songtype_id` INT UNSIGNED NOT NULL,
+  `species_id` INT NOT NULL,
+  `songtype_id` INT NOT NULL,
   `validated` INT NOT NULL,
   `correct` INT NOT NULL,
   `incorrect` INT NOT NULL,
@@ -16,14 +16,23 @@ CREATE TABLE `arbimon2`.`pattern_matching_user_statistics` (
 ALTER TABLE `arbimon2`.`pattern_matching_user_statistics`
 ADD CONSTRAINT `fk_pattern_matching_user_statistics_1`
     FOREIGN KEY (`user_id`) REFERENCES `arbimon2`.`users` (`user_id`)
-    ON DELETE NO ACTION ON UPDATE NO ACTION,
+    ON DELETE NO ACTION ON UPDATE NO ACTION
+;
+
+ALTER TABLE `arbimon2`.`pattern_matching_user_statistics`
 ADD CONSTRAINT `fk_pattern_matching_user_statistics_2`
     FOREIGN KEY (`project_id`) REFERENCES `arbimon2`.`projects` (`project_id`)
-    ON DELETE NO ACTION ON UPDATE NO ACTION,
+    ON DELETE NO ACTION ON UPDATE NO ACTION
+;
+
+ALTER TABLE `arbimon2`.`pattern_matching_user_statistics`
 ADD CONSTRAINT `fk_pattern_matching_user_statistics_3`
     FOREIGN KEY (`species_id`) REFERENCES `arbimon2`.`species` (`species_id`)
     ON DELETE NO ACTION ON UPDATE NO ACTION
+;
+
+ALTER TABLE `arbimon2`.`pattern_matching_user_statistics`
 ADD CONSTRAINT `fk_pattern_matching_user_statistics_4`
-    FOREIGN KEY (`songtypes_id`) REFERENCES `arbimon2`.`songtype` (`songtype_id`)
+    FOREIGN KEY (`songtype_id`) REFERENCES `arbimon2`.`songtypes` (`songtype_id`)
     ON DELETE NO ACTION ON UPDATE NO ACTION
 ;
