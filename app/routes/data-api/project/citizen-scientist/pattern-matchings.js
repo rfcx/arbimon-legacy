91bd681efc529459876661fb11dd9a7009df3755
gio <aleph.omega@gmail.com>
2019-05-16 03:53:16 -0400

    splitting cs routes into own folder, also fletching them out

---
+++ app/routes/data-api/project/citizen-scientist/pattern-matchings.js
@@ -0,0 +1,92 @@
+/* jshint node:true */
+"use strict";
+
+var q = require('q');
+var express = require('express');
+var router = express.Router();
+var model = require('../../../../model');
+
+
+/** Return a list of all the pattern matchings in a project.
+ */
+router.get('/', function(req, res, next) {
+    res.type('json');
+    model.patternMatchings.find({
+        project:req.project.project_id,
+        citizen_scientist:1,
+        deleted:0,
+        showUser:true,
+        showTemplate: true,
+        showPlaylist:true
+    }).then(function(count) {
+        res.json(count);
+    }).catch(next);
+});
+
+/** Return a pattern matching's data.
+ */
+router.get('/:patternMatching/details', function(req, res, next) {
+    res.type('json');
+    model.patternMatchings.findOne({
+        id: req.params.patternMatching,
+        showTemplate: true, showPlaylist:true, showCounts: true,
+        showSpecies: true,
+    }).then(function(pm) {
+        res.json(pm);
+    }).catch(next);
+});
+
+router.param('paging', function(req, res, next, paging){
+    const components = paging.split('_');
+    console.log('paging components', components);
+    req.paging = {
+        offset: (components[0] | 0),
+        limit: (components[1] | 0),
+    }
+    return next();
+});
+
+router.get('/:patternMatching/rois/:paging', function(req, res, next) {
+    res.type('json');
+    var user = req.session.user;
+    model.patternMatchings.getRoisForId({
+        patternMatchingId: req.params.patternMatching,
+        csValidationsFor: user.id,
+        limit: req.paging.limit || 100,
+        offset: req.paging.offset || 0,
+    }).then(function(rois) {
+        res.json(rois);
+    }).catch(next);
+});
+
+router.post('/:patternMatching/validate', function(req, res, next) {
+    res.type('json');
+    var user = req.session.user;
+    model.patternMatchings.validateCSRois(req.params.patternMatching, user.id, req.body.rois, req.body.validation).then(function(rois) {
+        res.json({
+            rois: req.body.rois,
+            validation: req.body.validation,
+        });
+    }).catch(next);
+});
+
+router.post('/:patternMatching/remove', function(req, res, next) {
+    res.type('json');
+
+    var project_id = req.project.project_id;
+
+    q.resolve().then(function(){
+        if(!req.haveAccess(project_id, "manage pattern matchings")){
+            throw new Error({
+                error: "You don't have permission to delete pattern matchings"
+            });
+        }
+    }).then(function(){
+        return model.patternMatchings.delete(req.params.patternMatching | 0);
+    }).then(function(){
+        res.json({ok: true});
+    }).catch(next);
+});
+
+
+module.exports = router;
---
+++ app/routes/data-api/project/citizen-scientist/stats.js
@@ -0,0 +1,69 @@
+/* jshint node:true */
+"use strict";
+
+var q = require('q');
+var express = require('express');
+var router = express.Router();
+var model = require('../../../../model');
+
+
+router.get('/classification', function(req, res, next) {
+    res.type('json');
+
+    var project_id = req.project.project_id;
+
+    q.resolve().then(function(){
+        return model.CitizenScientist.getClassificationStats({ project: project_id });
+    }).then(function(stats){
+        res.json({stats: stats});
+    }).catch(next);
+});
+
+router.get('/classification/:species', function(req, res, next) {
+    res.type('json');
+
+    var project_id = req.project.project_id;
+    var species_id = req.params.species | 0
+
+    q.resolve().then(function(){
+        return model.CitizenScientist.getClassificationStats({
+            project: project_id,
+            species: species_id,
+            groupByMatching: !!species_id,
+        });
+    }).then(function(stats){
+        res.json({stats: stats});
+    }).catch(next);
+});
+
+router.get('/user', function(req, res, next) {
+    res.type('json');
+
+    var project_id = req.project.project_id;
+
+    q.resolve().then(function(){
+        return model.CitizenScientist.getUserStats({ project: project_id });
+    }).then(function(stats){
+        res.json({stats: stats});
+    }).catch(next);
+});
+
+router.get('/user/:user', function(req, res, next) {
+    res.type('json');
+
+    var project_id = req.project.project_id;
+    var user_id = req.params.user | 0
+
+    q.resolve().then(function(){
+        return model.CitizenScientist.getUserStats({
+            project: project_id,
+            user: user_id,
+            groupByMatching: !!user_id,
+        });
+    }).then(function(stats){
+        res.json({stats: stats});
+    }).catch(next);
+});
+
+
+module.exports = router;
