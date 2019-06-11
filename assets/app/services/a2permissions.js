efc0d96190c204e4f1cf26f64e8f97676ed061e1
gio <aleph.omega@gmail.com>
2019-06-11 05:20:52 -0400

    adding permissions service to root scope

--- assets/app/services/a2permissions.js
+++ assets/app/services/a2permissions.js
@@ -2,17 +2,25 @@ angular.module('a2.permissions', [
     'a2.services',
     'ng-permissions'
 ])
+.run(function($rootScope, a2UserPermit){
+    $rootScope.userPermit = a2UserPermit;
+})
 .service('a2UserPermit', function($http, Project, $q, permits) {
     var permit = permits;
-    
+
     return {
+        all: permit.permissions,
+        isSuper: function(){
+            return permit.super;
+        },
         can: function(perm) {
             var allowed;
-            
+
             if(permit.permissions)
                 allowed = permit.permissions.indexOf(perm) !== -1;
-            
+
             return allowed || permit.super;
         }
     };
-});
+})
+;
