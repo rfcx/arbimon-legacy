652b3c925bb2f2c79d91787af8118a017192cb59
gio <aleph.omega@gmail.com>
2019-07-06 15:01:38 -0400

    adding pending in my stats. Also, %s are computed on number of votes that reached consensus

--- assets/app/app/citizen-scientist/my-stats/index.js
+++ assets/app/app/citizen-scientist/my-stats/index.js
@@ -18,25 +18,27 @@ angular.module('a2.citizen-scientist.my-stats', [
         this.loading = true;
         a2CitizenScientistService.getMyStats().then((function(data){
             this.loading = false;
+            var vars=['validated', 'consensus', 'non_consensus', 'pending', 'reached_th'];
+
             this.stats = data.stats.map(function(item, index) {
-                item.group_validated = data.groupStats[index].validated;
-                item.group_consensus = data.groupStats[index].consensus;
-                item.group_non_consensus = data.groupStats[index].non_consensus;
+                vars.forEach(function(_var) {
+                    item['group_' + _var] = data.groupStats[index][_var];
+                });
                 return item;
-            })
+            });
+
 
             this.overall = data.stats.reduce(function(_, item){
-                _.validated += item.validated;
-                _.consensus += item.consensus;
-                _.non_consensus += item.non_consensus;
-                _.group_validated += item.group_validated;
-                _.group_consensus += item.group_consensus;
-                _.group_non_consensus += item.group_non_consensus;
+                vars.forEach(function(_var) {
+                    _[_var] += item[_var];
+                    _['group_' + _var] += item['group_' + _var];
+                });
                 return _;
-            }, {
-                validated:0, consensus:0, non_consensus:0,
-                group_validated:0, group_consensus:0, group_non_consensus:0
-            });
+            }, vars.reduce(function(__, _var) {
+                __[_var] = 0;
+                __['group_' + _var] = 0;
+                return __;
+            }, {}));
 
             // compute overalls
         }).bind(this));
