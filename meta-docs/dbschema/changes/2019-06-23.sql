815df5e085561d12d774bd79c3ae62a10c710263
gio <aleph.omega@gmail.com>
2019-07-07 14:37:58 -0400

    removing uneeded query

--- meta-docs/dbschema/changes/2019-06-23.sql
+++ meta-docs/dbschema/changes/2019-06-23.sql
@@ -1,5 +1,2 @@
 ALTER TABLE `arbimon2`.`pattern_matchings`
 ADD COLUMN `consensus_number` INT(11) NOT NULL DEFAULT 3 AFTER `citizen_scientist`;
-
-ALTER TABLE `arbimon2`.`projects`
-DROP COLUMN `citizen_scientist_validation_consensus_number`;
