92551ed2c855ef0fee5d13f2f04acb1df137cbb0
Michael Haas <micha2718l@gmail.com>
2019-07-12 18:35:54 -0500

    cnn a2-service, PM with replaces

---
+++ assets/app/a2services/cnn-service.js
@@ -0,0 +1,53 @@
+angular.module('a2.srv.cnn', [
+    'a2.srv.project',
+    'humane'
+])
+.factory('a2CNN', function($q, $http, Project, notify) {
+    var saveData = null;
+
+    return {
+        saveState : function(data) {
+            saveData = data;
+        },
+        getState : function() {
+            return saveData;
+        },
+        list: function(callback) {
+            return $http.get('/api/project/'+Project.getUrl()+'/cnn').then(function(response){
+                return response.data;
+            }).catch(notify.serverError);
+        },
+        getDetailsFor: function(cnnId) {
+            return $http.get('/api/project/' + Project.getUrl() + '/cnn/' + cnnId + '/details').then(function(response){
+                return response.data;
+            });
+        },
+        getRoisFor: function(cnnId, limit, offset) {
+            return $http.get('/api/project/' + Project.getUrl() + '/cnn/' + cnnId + '/rois/' + (offset||0) + '_' + (limit||0)).then(function(response){
+                return response.data;
+            });
+        },
+        getExportUrl: function(params){
+            return '/api/project/' + Project.getUrl() + '/cnn/' + params.patternMatching + '/rois.csv';
+        },
+        validateRois: function(cnnId, rois, validation) {
+            return $http.post('/api/project/' + Project.getUrl() + '/cnn/' + cnnId + '/validate', {
+                rois: rois,
+                validation: validation,
+            }).then(function(response){
+                return response.data;
+            });
+        },
+        create: function(data) {
+            return $http.post('/api/project/'+Project.getUrl()+'/cnn/new', data).then(function(response){
+                return response.data;
+            });
+        },
+        delete: function(cnnId) {
+            return $http.post('/api/project/' + Project.getUrl() + '/cnn/' + cnnId + '/remove').then(function(response){
+                return response.data;
+            });
+        },
+    };
+})
+;
