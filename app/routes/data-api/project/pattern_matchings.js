432c639c627dc855db7ff1ea0be0069e7a554c7b
gio <aleph.omega@gmail.com>
2019-08-03 16:30:15 -0400

    adding site-index route

--- app/routes/data-api/project/pattern_matchings.js
+++ app/routes/data-api/project/pattern_matchings.js
@@ -65,6 +65,22 @@ router.get('/:patternMatching/rois/:paging', function(req, res, next) {
     }).catch(next);
 });
 
+router.get('/:patternMatching/site-index', function(req, res, next) {
+    res.type('json');
+    model.patternMatchings.getRoisForId({
+        patternMatchingId: req.params.patternMatching,
+        perSiteCount: true,
+        wherePresent: req.query.search == 'present',
+        whereNotPresent: req.query.search == 'not_present',
+        whereUnvalidated: req.query.search == 'unvalidated',
+    }).then(function(rois) {
+        res.json(rois.reduce(function(_, item){
+            item.offset = _.accum;
+            _.accum += item.count;
+            return _;
+        }, {list:rois, accum:0}).list);
+    }).catch(next);
+});
 
 router.get('/:patternMatching/rois.csv', function(req, res, next) {
     if(req.query.out=="text"){
