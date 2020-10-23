eb5d367ced6d0dacfaf00c26ba23c9c3f5078cc6
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2020-10-23 12:37:31 +0300

    WB-2100 create api endpoint and list of audio event detections clustering jobs

--- assets/app/app/analysis/index.js
+++ assets/app/app/analysis/index.js
@@ -6,7 +6,7 @@ angular.module('a2.analysis', [
     'a2.analysis.cnn',
     'a2.analysis.soundscapes',
     'a2.analysis.audio-event-detection',
-    'a2.analysis.clustering-model',
+    'a2.analysis.audio-event-detections-clustering',
     'ui.router',
     'ct.ui.router.extras'
 ]).config(function($stateProvider, $urlRouterProvider) {
