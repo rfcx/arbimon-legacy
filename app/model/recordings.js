8eab4004150c8fd271c29de6afbd83d31346de89
RatreeOchn <ratree.ochn@gmail.com>
2021-07-12 18:35:24 +0700

    Update to use if conduction in the query

--- app/model/recordings.js
+++ app/model/recordings.js
@@ -787,7 +787,7 @@ var Recordings = {
                     queryHandler(
                         "INSERT INTO recording_validations(recording_id, user_id, species_id, songtype_id, present, project_id, present_review) \n" +
                         " VALUES (" + dbpool.escape([valobj.recording, valobj.user, valobj.species, valobj.songtype, valobj.val, valobj.project_id, 1]) + ") \n" +
-                        " ON DUPLICATE KEY UPDATE present = VALUES(present), present_review = present_review + 1", function(err, data){
+                        " ON DUPLICATE KEY UPDATE present = VALUES(present), present_review = CASE WHEN present = 0 THEN 0 ELSE present_review + 1 END", function(err, data){
                         if (err) { callback(err); return; }
                         callback(null, valobj);
                     });
