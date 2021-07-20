f7dd478a3f4382fb525f3b8a481acfe7162ede01
RatreeOchn <ratree.ochn@gmail.com>
2021-07-20 22:19:28 +0700

    Fix show Present and Absent

--- assets/app/app/visualizer/validator.js
+++ assets/app/app/visualizer/validator.js
@@ -23,7 +23,7 @@ angular.module('a2.speciesValidator', ['a2.utils', 'a2.infotags'])
             
             var add_validation = function(validation){
                 var key     = [validation.species, validation.songtype].join('-');
-                var present = Object.values(validation);
+                var present = Object.values({present: validation.present, presentReview: validation.presentReview});
                 $scope.validations[key] = present;
             };
 
@@ -133,7 +133,7 @@ angular.module('a2.speciesValidator', ['a2.utils', 'a2.infotags'])
                                 delete $scope.validations[key];
                             }
                             else {
-                                $scope.validations[key] = validation;
+                                $scope.validations[key] = Object.values({present: validation.val, presentReview: 0});
                             }
                         });
                     });
@@ -157,8 +157,8 @@ angular.module('a2.speciesValidator', ['a2.utils', 'a2.infotags'])
                     return;
                 }
                 else {
-                    if (val[4] == 1 || val[5] > 0) {
-                        val_options[1].showDropdown = (val[5] > 0)
+                    if (val[0] == 1 || val[1] > 0) {
+                        val_options[1].showDropdown = (val[1] > 0)
                         return val_options[1];
                     } else {
                         return val_options[2];
