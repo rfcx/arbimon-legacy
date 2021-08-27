49f3a0e4348da32babc94df85826359daeea51b6
Antony Harfield <antonyharfield@gmail.com>
2021-08-27 20:37:31 +0100

    Add missing present column change for recording_validations

--- scripts/db/018-add-present-review.sql
+++ scripts/db/018-add-present-review.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `arbimon2`.`recording_validations` 
 ADD COLUMN `present_review` INT NOT NULL DEFAULT 0 AFTER `present`;
+ALTER TABLE recording_validations MODIFY present TINYINT(1) NULL;
