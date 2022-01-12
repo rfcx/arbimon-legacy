363ba5e8d054b0c60af020c12eb104dd8bce4e1d
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2022-01-13 01:40:20 +0300

    #852 Added a timeout after user presses the last button

--- assets/app/app/audiodata/species.js
+++ assets/app/app/audiodata/species.js
@@ -1,47 +1,47 @@
 angular.module('a2.audiodata.species', [
-    'a2.services', 
-    'a2.directives', 
+    'a2.services',
+    'a2.directives',
     'ui.bootstrap',
     'humane'
 ])
 .controller('SpeciesCtrl', function($scope, Project, $modal, notify, a2UserPermit) {
     $scope.loading = true;
     $scope.selected = {};
-    
+
     Project.getClasses(function(classes){
         $scope.classes = classes;
         $scope.loading = false;
     });
-    
+
     Project.getInfo(function(info){
         $scope.project = info;
     });
-    
-    
+
+
     $scope.add = function() {
-        
+
         if(!a2UserPermit.can("manage project species")) {
             notify.log("You do not have permission to add species");
             return;
         }
-        
+
         var modalInstance = $modal.open({
             templateUrl: '/app/audiodata/select-species.html',
             controller: 'SelectSpeciesCtrl',
             size: 'lg',
         });
-        
+
         modalInstance.result.then(function(selected) {
-            
+
             var cls = {
                 species: selected.species.scientific_name,
                 songtype: selected.song.name
             };
-            
+
             Project.addClass(cls)
                 .success(function(result){
                     notify.log(selected.species.scientific_name + ' ' + selected.song.name +" added to project");
-                    
+
                     Project.getClasses(function(classes){
                         $scope.classes = classes;
                     });
@@ -52,46 +52,46 @@ angular.module('a2.audiodata.species', [
                     else
                         notify.serverError();
                 });
-            
+
         });
     };
-    
+
     $scope.del = function() {
         if(!$scope.checked || !$scope.checked.length)
             return;
-        
+
         if(!a2UserPermit.can("manage project species")) {
             notify.log("You do not have permission to remove species");
             return;
         }
-        
+
         var speciesClasses = $scope.checked.map(function(row) {
             return '"'+row.species_name +' | ' + row.songtype_name+'"';
         });
-        
+
         var message = ["You are about to delete the following project species: "];
         var message2 = ["Are you sure?"];
-        
+
         $scope.popup = {
             messages: message.concat(speciesClasses, message2),
             btnOk: "Yes, do it!",
             btnCancel: "No",
         };
-        
+
         var modalInstance = $modal.open({
             templateUrl: '/common/templates/pop-up.html',
             scope: $scope
         });
-        
+
         modalInstance.result.then(function() {
             var classesIds = $scope.checked.map(function(row) {
                 return row.id;
             });
-            
+
             var params = {
                 project_classes: classesIds
             };
-            
+
             Project.removeClasses(params)
                 .success(function(result) {
                     Project.getClasses(function(classes){
@@ -105,25 +105,24 @@ angular.module('a2.audiodata.species', [
                         notify.serverError();
                 });
         });
-            
+
     };
 })
 .controller('SelectSpeciesCtrl', function($scope, Species, Songtypes) {
-    
+    $scope.timeout = null;
     Songtypes.get(function(songs) {
         $scope.songtypes = songs;
     });
-    
-        
     $scope.searchSpecies = function() {
         if($scope.search === "") {
             $scope.species = [];
             return;
         }
-    
-        Species.search($scope.search, function(results){
-            $scope.species = results;
-        });
+        clearTimeout($scope.timeout);
+        $scope.timeout = setTimeout(() => {
+            Species.search($scope.search, function(results){
+                $scope.species = results;
+            });
+        }, 500)
     };
-})
-;
+});
