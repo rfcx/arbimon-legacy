690acaee72f051c2ce8c58c2a786136192cb467f
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2022-10-06 12:55:28 +0400

    Check github actions code

---
+++ jobs/arbimon-recording-delete-job/build/README.md
@@ -0,0 +1,29 @@
+# Deployment
+
+Overview:
+- arbimon-recording-delete-job is built and deployed by Github Actions.
+- Deployment is triggered by push to testing/master. All configuration is in the sub-folders `testing` and `production` (corresponding to a Kubernetes namespace).
+- Secrets are stored on Kubernetes only in `arbimon-recording-delete-job-secrets` file of each namespace.
+- Deployment notifications are posted on Slack #alerts-deployment
+
+
+## Test deployment locally
+
+Requires Docker.
+
+1.  Build the image
+    ```
+    docker build -t arbimon-recording-delete-job -f build/Dockerfile .
+    ```
+
+2.  Run the app
+    ```
+    docker run -it --env-file .env --rm arbimon-recording-delete-job
+    ```
+
+
+## Kubernetes configuration
+
+Each sub-folder matches the name of a namespace in Kubernetes. The app name is `arbimon-recording-delete-job` in each namespace. For each namespace folder:
+
+- deployment.yaml - set the resources
---
+++ jobs/arbimon-recording-delete-job/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const db = require('./utils/db')
+const moment = require('moment')
+const { deleteRecordings } = require('../services/recordings')
+const { reportStats } = require('../services/stats')
+
+async function main () {
+  try {
+    console.log('arbimon-recording-delete job started')
+  
+    const dateByCondition = moment.utc().subtract(30, 'days').format('YYYY-MM-DD HH:mm:ss')
+    const where = `deleted_at < '${dateByCondition}'`
+    const limit = 10000
+    let toProcess = true
+    let countOfDeletedRows = 0
+
+    while (toProcess === true) {
+      // Delete recordings
+     const result =  await deleteRecordings({
+        where,
+        orderBy: 'deleted_at ASC',
+        limit
+     })
+     toProcess = result.affectedRows > 0
+     countOfDeletedRows += result.affectedRows
+    }
+    await db.closeAll()
+
+    // Send report to the slack biodiversity-dev
+    await reportStats(countOfDeletedRows)
+    console.log('arbimon-recording-delete job finished')
+  } catch (e) {
+    console.error(e)
+    await db.closeAll()
+  }
+}
+
+main()
