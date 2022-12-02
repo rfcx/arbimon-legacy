f31ab7b2f171208beda3cbb34639d7ec172ced61
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2022-12-02 15:19:13 +0400

    Add temp log to check auth0 env on staging side

--- app/model/auth0.js
+++ app/model/auth0.js
@@ -1,7 +1,7 @@
 const config = require('../config');
 const jsonwebtoken = require('jsonwebtoken');
 const auth0Config = config('auth0');
-const auth0BackendConfig = config('auth0-backend');
+const auth0BackendConfig = config('auth0_backend');
 const request = require('request');
 const { promisify } = require('util');
 const rp = promisify(request);
