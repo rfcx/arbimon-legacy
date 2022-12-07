204cb359f682bf44d5c59e1f2255ffde7dd247c5
Antony Harfield <antonyharfield@hotmail.com>
2022-12-07 22:47:53 +0000

    Fix database seeds failing

--- scripts/db/034-add-deleted-to-aed-ans-clustering.sql
+++ scripts/db/034-add-deleted-to-aed-ans-clustering.sql
@@ -3,13 +3,3 @@ ADD COLUMN `deleted` tinyint(1) NOT NULL DEFAULT '0';
 
 ALTER TABLE `arbimon2`.`job_params_audio_event_clustering`
 ADD COLUMN `deleted` tinyint(1) NOT NULL DEFAULT '0';
-
-INSERT INTO `arbimon2`.`permissions` (name, description, security_level)
-VALUES ('manage AED and Clustering job', 'user can create, delete and validate AED and Clustering job', 0);
-
-
-INSERT INTO `arbimon2`.`role_permissions` (role_id, permission_id)
-VALUES
-    (1, 25),
-    (4, 25),
-    (6, 25);
