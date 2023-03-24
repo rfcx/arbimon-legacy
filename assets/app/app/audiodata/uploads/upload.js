04210ad140393a52c10f4f3297021c76675df89a
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2023-03-24 20:42:45 +0400

    #1183 Show error for 0 byte file

--- assets/app/app/audiodata/uploads/upload.js
+++ assets/app/app/audiodata/uploads/upload.js
@@ -54,6 +54,12 @@ angular.module('a2.audiodata.uploads.upload', [
 
             var item = $scope.uploader.queue[index];
 
+            if (!item.file.size) {
+                item.isError = true
+                item.errorMsg = "Error of the file size"
+                return
+            }
+
             var next = function() {
                 index++;
                 _verifyAndUpload();
