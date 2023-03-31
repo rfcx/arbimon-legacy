3ae4199389c2417c78741225f7e85b1992298cc7
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2023-03-31 21:44:40 +0400

    #1202 Fix the timezone offset using STD on the sites page

--- app/utils/date-helper.js
+++ app/utils/date-helper.js
@@ -3,7 +3,11 @@ const moment = require('moment-timezone')
 const dateHelper = {
     getNonDlsOffset: function(timezone) {
         // Standart time (STD) offset
-        return moment('2022-12-01T00:00:00').tz(timezone).utcOffset() / 60
+        const janOffset = moment({M:0, d:1}).tz(timezone).utcOffset();
+        const julOffset = moment({M:6, d:1}).tz(timezone).utcOffset();
+        const stdOffset = Math.min(janOffset, julOffset);
+        const nowWithoutDST = moment().tz(timezone).utcOffset(stdOffset);
+        return nowWithoutDST.format('Z');
     },
     getNonDlsDatetime: function(datetime, timezone, format) {
         // get datetime converted with STD offset
