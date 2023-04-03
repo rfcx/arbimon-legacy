2a79d74634c01d894a17a8b8ac8249e35bf86003
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2023-04-03 17:38:53 +0400

    #1202 Try to fix std timezone for the timezones where dst in the winter time

--- app/utils/date-helper.js
+++ app/utils/date-helper.js
@@ -3,18 +3,15 @@ const moment = require('moment-timezone')
 const dateHelper = {
     getNonDlsOffset: function(timezone) {
         // Standart time (STD) offset
-        const janOffset = moment({M:0, d:1}).tz(timezone).utcOffset();
-        const julOffset = moment({M:6, d:1}).tz(timezone).utcOffset();
-        const stdOffset = Math.min(janOffset, julOffset);
-        const nowWithoutDST = moment().tz(timezone).utcOffset(stdOffset);
-        return nowWithoutDST.format('Z');
+        const dates = ['2022-01-02', '2022-02-02', '2022-03-02', '2022-04-02', '2022-05-02', '2022-06-02', '2022-06-02', '2022-07-02', '2022-08-02', '2022-09-02', '2022-10-02', '2022-11-02', '2022-12-02']
+        const dsdDay = dates.find(date => moment(date).tz(timezone).isDST())
+        const stdDay = dates.find(date => !moment(date).tz(timezone).isDST())
+        if (timezone === 'Europe/Dublin') return moment(dsdDay).tz(timezone).utcOffset();
+        return moment(stdDay).tz(timezone).utcOffset();
     },
     getNonDlsDatetime: function(datetime, timezone, format) {
         // get datetime converted with STD offset
-        const isDST = moment(datetime).tz(timezone).isDST()
-        return isDST
-            ? moment(datetime).tz(timezone).utcOffset(this.getNonDlsOffset(timezone), true).format(format)
-            : moment(datetime).tz(timezone).format(format)
+        return moment(datetime).tz(timezone).utcOffset(this.getNonDlsOffset(timezone), true).format(format)
     }
 }
 
