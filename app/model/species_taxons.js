5eb7d7b1b6cf210478f29c8e370786adba902e99
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2023-09-07 17:19:30 +0400

    #1319 Order taxons by taxon name

--- app/model/species_taxons.js
+++ app/model/species_taxons.js
@@ -5,7 +5,8 @@ const queryHandler = dbpool.queryHandler;
 const SpeciesTaxons = {
     listAll: function(callback) {
         const q = `SELECT taxon_id as id, taxon
-            FROM species_taxons WHERE enabled = 1`;
+            FROM species_taxons WHERE enabled = 1
+            ORDER BY taxon ASC`;
 
         queryHandler(q, callback);
     }
