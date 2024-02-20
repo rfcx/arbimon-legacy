5a878290e44ba57363c59c6bd35e6f0d49097950
ratree <ratree.ochn@gmail.com>
2024-02-21 00:17:41 +0700

    #1444 Add toggle to the UI as per the design. On change, update the value of public_templates_enabled in the projects table.

--- assets/app/app/analysis/patternmatching/index.js
+++ assets/app/app/analysis/patternmatching/index.js
@@ -1,3 +1,30 @@
+angular.module('uiSwitch', [])
+.directive('switch', function(){
+    return {
+      restrict: 'AE'
+    , replace: true
+    , transclude: true
+    , template: function(element, attrs) {
+        var html = '';
+        html += '<span';
+        html +=   ' class="switch' + (attrs.class ? ' ' + attrs.class : '') + '"';
+        html +=   attrs.ngModel ? ' ng-click="' + attrs.disabled + ' ? ' + attrs.ngModel + ' : ' + attrs.ngModel + '=!' + attrs.ngModel + (attrs.ngChange ? '; ' + attrs.ngChange + '()"' : '"') : '';
+        html +=   ' ng-class="{ checked:' + attrs.ngModel + ', disabled:' + attrs.disabled + ' }"';
+        html +=   '>';
+        html +=   '<small></small>';
+        html +=   '<input type="checkbox"';
+        html +=     attrs.id ? ' id="' + attrs.id + '"' : '';
+        html +=     attrs.name ? ' name="' + attrs.name + '"' : '';
+        html +=     attrs.ngModel ? ' ng-model="' + attrs.ngModel + '"' : '';
+        html +=     ' style="display:none" />';
+        html +=     '<span class="switch-text">'; /*adding new container for switch text*/
+        html +=     attrs.on ? '<span class="on">'+attrs.on+'</span>' : ''; /*switch text on value set by user in directive html markup*/
+        html +=     attrs.off ? '<span class="off">'+attrs.off + '</span>' : ' ';  /*switch text off value set by user in directive html markup*/
+        html += '</span>';
+        return html;
+      }
+    }
+  })
 angular.module('a2.analysis.patternmatching', [
     'ui.bootstrap',
     'a2.directive.audio-bar',
@@ -6,6 +33,7 @@ angular.module('a2.analysis.patternmatching', [
     'a2.permissions',
     'humane',
     'c3-charts',
+    'uiSwitch'
 ])
 .config(function($stateProvider, $urlRouterProvider) {
     $stateProvider.state('analysis.disabled-patternmatching', {
@@ -40,6 +68,8 @@ angular.module('a2.analysis.patternmatching', [
     Project,
     SpeciesTaxons
 ) {
+    $scope.onOff = true;
+
     $scope.selectedPatternMatchingId = $stateParams.patternMatchingId;
     $scope.loading = { rows: false, showRefreshBtn: false };
     $scope.pagination = {
