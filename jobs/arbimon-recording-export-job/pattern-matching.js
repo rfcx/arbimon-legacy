e56ee34416a0a905c8e9fdfd75700ca576b1e40b
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2024-05-10 23:00:27 +0400

    #1538 Improve PM export job

--- jobs/arbimon-recording-export-job/pattern-matching.js
+++ jobs/arbimon-recording-export-job/pattern-matching.js
@@ -3,8 +3,9 @@ const path = require('path')
 const fs = require('fs')
 const stream = require('stream');
 const csv_stringify = require('csv-stringify');
+const archiver = require('archiver');
 const { getPmRois, getProjectPMJobs, getProjectSites } = require('../services/pattern-matching')
-const { zipDirectory } = require('../services/file-helper')
+const { zipDirectory, nameToUrl, isLegacy } = require('../services/file-helper')
 const { getSignedUrl } = require('../services/storage')
 const { getRecordingByIds } = require('../services/recordings')
 
@@ -15,6 +16,17 @@ const exportReportType = 'Pattern Matchings';
 const exportReportJob = `Arbimon Export ${exportReportType} job`
 const tmpFilePath = 'jobs/arbimon-recording-export-job/tmpfilecache'
 
+const archive = archiver('zip', { zlib: { level: 9 }});
+const outputStreamFile = fs.createWriteStream(__dirname + '/pattern-matching-export.zip');
+
+archive.on('error', (err) => {
+  console.log('\n\n<- fileToZipDirectory error', err)
+})
+archive.on('end', () => {
+  console.log('\n\n<- fileToZipDirectory added')
+})
+archive.pipe(outputStreamFile)
+
 async function collectData (filters, cb) {
   await exportAllPmJobs(filters.project_id, async (err, data) => {
     if (err) {
@@ -22,6 +34,7 @@ async function collectData (filters, cb) {
       return cb(err)
     }
     console.log(`${exportReportJob}: finished collecting jobs`)
+    archive.finalize();
     cb(null, null)
   }).catch((e) => {
     console.err('Error export PM', e)
@@ -33,8 +46,13 @@ async function buildPMFolder() {
   await zipDirectory(tmpFilePath, 'jobs/arbimon-recording-export-job/pattern-matching-export.zip')
 }
 
-function nameToUrl (name) {
-  return name.replace(/[^a-z0-9A-Z-]/g, '-').replace(/-+/g,'-').replace(/(^-)|(-$)/g, '').toLowerCase()
+async function fileToZipDirectory (sourceFilePath, sourceFileName) {
+  return new Promise((resolve, reject) => {
+    archive.append(fs.createReadStream(sourceFilePath)
+      .on('end', function() {console.log('End fileToZipDirectory.'); resolve()})
+      .on('error', function(err) {console.log('Error fileToZipDirectory.', err); reject()})
+    , { name: sourceFileName })
+  });
 }
 
 async function exportAllPmJobs (projectId, cb) {
@@ -42,33 +60,41 @@ async function exportAllPmJobs (projectId, cb) {
     console.log(`${exportReportJob} started`)
     const projectSites = await getProjectSites({projectId})
     const projectPMJobs = await getProjectPMJobs({projectId})
+    await exportAllPmJobsCsv(projectPMJobs)
     for (let job of projectPMJobs) {
-      const filename = `${job.jobId}-${nameToUrl(job.jobName)}.csv`;
-      const filePath = path.join(tmpFilePath, filename)
+      const fileName = `${job.job_id}-${nameToUrl(job.job_name)}.csv`;
+      const filePath = path.join(tmpFilePath, fileName)
       const targetFile = fs.createWriteStream(filePath, { flags: 'a' })
-      console.log('targetFile start', filename)
+      console.log('next PM job start:', fileName)
       const limit = 10000;
       let index = 0
       let toProcess = true;
       let isFirstChunk = true
       while (toProcess === true) {
-        console.log('next chunk for', filename)
+        console.log('next chunk for', fileName)
         const queryResult =  await getPmRois({
           projectId,
-          jobId: job.jobId,
+          jobId: job.job_id,
           limit,
           offset: limit * index
         });
         toProcess = queryResult.length > 0;
         if (toProcess) {
-          console.log('Arbimon Export PM job: writing chunk', queryResult.length)
+          console.log('Arbimon Export PM job: writing chunk: rows length', queryResult.length)
           await writeChunk(queryResult, targetFile, projectSites, isFirstChunk)
         }
         isFirstChunk = false
         index++
       }
-      console.log('targetFile end', filename)
       targetFile.end()
+      console.log('PM job end:', fileName)
+      await fileToZipDirectory(`${__dirname}/${filePath}`, fileName)
+        .then(() => {
+          fs.unlink(`${__dirname}/${filePath}`, function(err){
+            if(err) return console.error('error file delete', err);
+            console.log('file deleted successfully');
+          })
+        })
     }
     cb(null, null)
   } catch (e) {
@@ -77,8 +103,50 @@ async function exportAllPmJobs (projectId, cb) {
   }
 }
 
-function isLegacy (uri) {
-    return uri.startsWith('project_')
+async function exportAllPmJobsCsv (results) {
+  const fileName = '_pattern-matchings.csv';
+  const filePath = path.join(tmpFilePath, fileName)
+  const targetFileSummaryData = fs.createWriteStream(filePath, { flags: 'a' })
+  return new Promise(async function (resolve, reject) {
+    let fields = [];
+    results.forEach(result => {
+        fields.push(...Object.keys(result).filter(f => !fields.includes(f)))
+    });
+
+    let datastream = new stream.Readable({objectMode: true});
+    let _buf = []
+
+    for (let result of results) {
+      fields.forEach(f => {
+        if (result[f] === undefined || result[f] === null) {
+          result[f] = '---'}
+        }
+      )
+      datastream.push(result)
+    }
+    datastream.push(null);
+
+    datastream.on('data', (d) => {
+      _buf.push(Object.values(d))
+    })
+
+    datastream.on('end', async () => {
+      csv_stringify(_buf, { header: true, columns: fields }, async (err, data) => {
+        if (err) {
+          reject(err)
+        }
+        targetFileSummaryData.write(data)
+        targetFileSummaryData.end()
+        await fileToZipDirectory(`${__dirname}/tmpfilecache/_pattern-matchings.csv`, fileName).then(() => {
+          fs.unlink(`${__dirname}/tmpfilecache/_pattern-matchings.csv`, function(err) {
+            if (err) return console.error('error targetFileSummaryData delete', err);
+            console.log('targetFileSummaryData deleted successfully');
+          })
+        })
+        resolve()
+      })
+    })
+  })
 }
 
 async function writeChunk (results, targetFile, projectSites, isFirstChunk) {
@@ -125,9 +193,9 @@ async function writeChunk (results, targetFile, projectSites, isFirstChunk) {
           if (err) {
             reject(err)
           }
-          targetFile.write(data)
           console.log('targetFile write', _buf.length)
-          resolve()
+          targetFile.write(data);
+          return resolve();
         })
       })
   })
--- jobs/services/pattern-matching.js
+++ jobs/services/pattern-matching.js
@@ -3,13 +3,11 @@ const mysql = require('../db/mysql')
 async function getPmRois (options = {}) {
   const connection = await mysql.getConnection()
   const sql = `
-    select /*+ MAX_EXECUTION_TIME(360000) */  pm.pattern_matching_id job_id, pm.name job_name, pm.template_id, sp.species_id, sp.scientific_name, st.songtype,
-    pmr.x1, pmr.y1, pmr.x2, pmr.y2, pmr.score, pmr.validated, pmr.denorm_site_id as site_id,
-    pmr.recording_id, pmr.denorm_recording_datetime recording_local_time
+    select /*+ MAX_EXECUTION_TIME(360000) */ TRUNCATE(pmr.x1, 3) x1, TRUNCATE(pmr.y1, 3) y1, TRUNCATE(pmr.x2, 3) x2, TRUNCATE(pmr.y2, 3) y2,
+      TRUNCATE(pmr.score, 3) score, pmr.validated, pmr.denorm_site_id as site_id,
+      pmr.recording_id, pmr.denorm_recording_datetime recording_local_time
     from pattern_matching_rois pmr
       join pattern_matchings pm on pm.pattern_matching_id = pmr.pattern_matching_id
-      join species sp on pmr.species_id = sp.species_id
-      join songtypes st on pmr.songtype_id = st.songtype_id
     where pm.project_id = ${options.projectId} and pm.pattern_matching_id = ${options.jobId} and pm.deleted = 0
       limit ${options.limit} offset ${options.offset}
     ;
@@ -21,9 +19,11 @@ async function getPmRois (options = {}) {
 async function getProjectPMJobs (options = {}) {
   const connection = await mysql.getConnection()
   const sql = `
-    select pm.pattern_matching_id jobId, pm.name jobName
+    select pm.pattern_matching_id job_id, pm.name job_name, pm.template_id, sp.species_id, sp.scientific_name, st.songtype
     from pattern_matchings pm
-    join jobs j ON pm.job_id = j.job_id
+      join jobs j ON pm.job_id = j.job_id
+      join species sp on pm.species_id = sp.species_id
+      join songtypes st on pm.songtype_id = st.songtype_id
     where pm.project_id = ${options.projectId} and pm.deleted = 0 and j.state = 'completed'
     ;
   `
