d9f3a74a765652a0c14065fd1f19abc4c377ddcf
ratree <ratree.ochn@gmail.com>
2025-02-21 14:58:05 +0700

    #2156 Add tooltip on delete button and change color icon

--- assets/app/app/jobs/index.js
+++ assets/app/app/jobs/index.js
@@ -54,7 +54,7 @@ angular.module('a2.jobs', [
     };
 
     JobsData.getJobTypes().success(function(data) {
-        var colors = ['#1482f8', '#df3627', '#40af3b', '#9f51bf', '#d37528', '#ffff00', '#5bc0de', '#53ff4033'];
+        var colors = ['#1482f8', '#df3627', '#40af3b', '#9f51bf', '#d37528', '#ffff00', '#5bc0de', '#80BDFF'];
         var job_types_id = [1, 2, 4, 6, 7, 8, 9, 10];
         
         var job_types = data.filter(function(type) {
