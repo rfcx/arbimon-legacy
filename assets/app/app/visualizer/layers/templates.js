764c6abdfa86f7d41e9fa2926c5d574aa6ad72f5
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2025-07-03 02:02:37 +0400

    #1692 Add warning when large template created

--- assets/app/app/visualizer/layers/templates.js
+++ assets/app/app/visualizer/layers/templates.js
@@ -30,6 +30,7 @@ angular.module('a2.visualizer.layers.templates', [
     self.toggleSpeciesSelect = false;
     self.toggleSongtypeSelect = false;
     self.resetSearch = false;
+    self.showTemplateNotification = false;
     self.userSearch = '';
     self.allSpecies = [];
     self.songtypes = [];
@@ -131,6 +132,9 @@ angular.module('a2.visualizer.layers.templates', [
     self.selectSongtype = function(song) {
         self.classToAdd.songtype = song.name
     }
+    self.closeTemplateNotification = function() {
+        self.showTemplateNotification = false;
+    }
     self.backToSelectSpecies = function() {
         self.toggleSpeciesSelect = true;
         self.toggleSongtypeSelect = false;
@@ -223,9 +227,20 @@ angular.module('a2.visualizer.layers.templates', [
         get_placeholder_name: function(){
             return this.project_class ? (this.project_class.species_name + ' ' + this.project_class.songtype_name) : 'Template Name';
         },
-        submit: function(){
+        submit: function() {
+            self.showTemplateNotification = false;
             if(!a2UserPermit.can('manage templates')) {
                 notify.error('You do not have permission to add a template');
+                return;
+            }
+            console.log('this.recording', this.recording, this.roi, $scope.visobject)
+
+            const frequency = $scope.visobject.span;
+            const roiX = this.roi.x2 - this.roi.x1;
+            const roiY = this.roi.y2 - this.roi.y1;
+
+            if ((roiX > 5) || (roiY > (frequency/2))) {
+                self.showTemplateNotification = true;
                 return;
             }
 
@@ -257,79 +272,4 @@ angular.module('a2.visualizer.layers.templates', [
 
     $scope.$watch('visobject', self.updateState);
 })
-// .controller('a2VisualizerAddTemplateModalController', function($scope, $modalInstance, Project, a2Templates){
-//     $scope.data = {
-//         name : '',
-//         type : null
-//     };
-//
-//     $scope.loadingClasses = true;
-//
-//     Project.getClasses(function(project_classes){
-//         $scope.project_classes = project_classes;
-//         $scope.loadingClasses = false;
-//     });
-//
-//     $scope.loadingTypes = true;
-//
-//     a2Templates.getTypes(function(template_types){
-//         $scope.template_types = template_types;
-//         if(template_types && template_types.length == 1) {
-//             $scope.data.type = template_types[0];
-//             $scope.loadingTypes = false;
-//         }
-//     });
-//
-//     $scope.ok = function(){
-//         $scope.creating = true;
-//         $scope.validation = {
-//             count:0
-//         };
-//
-//         var template_data = {};
-//
-//         if($scope.data.name){
-//             template_data.name = $scope.data.name;
-//         }
-//         else {
-//             $scope.validation.name = "Training set name is required.";
-//             $scope.validation.count++;
-//         }
-//
-//         if($scope.data.type && $scope.data.type.id){
-//             template_data.type = $scope.data.type.identifier;
-//         }
-//         else {
-//             $scope.validation.type = "Training set type is required.";
-//             $scope.validation.count++;
-//         }
-//
-//         if($scope.data.class) {
-//             template_data.class = $scope.data.class.id;
-//         }
-//         else {
-//             $scope.validation.class = "Species sound is required.";
-//             $scope.validation.count++;
-//         }
-//
-//         // $scope.form_data=template_data;
-//
-//         if($scope.validation.count ===  0) {
-//             a2Templates.add(template_data, function(new_template) {
-//                 if(new_template.error) {
-//
-//                     var field = new_template.field || 'error';
-//
-//                     $scope.validation[field] = new_template.error;
-//
-//                     return;
-//                 }
-//                 $modalInstance.close(new_template);
-//             });
-//         }
-//         else {
-//             $scope.creating = false;
-//         }
-//     };
-// })
 ;
